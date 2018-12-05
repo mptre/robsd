4c2f8b3ac419a289f2766aa48b13822b9382a35e
Anton Lindqvist <anton@basename.se>
2018-12-05 07:41:42 +0100

    Patch silently and remove left overs

--- patch.sh
+++ patch.sh
@@ -1,4 +1,4 @@
 [ -z "$DIFF" ] && exit 0
 
 cp "$DIFF" "${LOGDIR}/src.diff"
-(cd "$BSDSRCDIR" && patch -E) <"$DIFF"
+(cd "$BSDSRCDIR" && patch -Es) <"$DIFF"
--- revert.sh
+++ revert.sh
@@ -1,3 +1,4 @@
 [ -e "${LOGDIR}/src.diff" ] || exit 0
 
-(cd "$BSDSRCDIR" && patch -ER) <"${LOGDIR}/src.diff"
+(cd "$BSDSRCDIR" && patch -ERs) <"${LOGDIR}/src.diff"
+find "$BSDSRCDIR" -type f -name '*.orig' -o -name '*.rej' | xargs -rt rm
