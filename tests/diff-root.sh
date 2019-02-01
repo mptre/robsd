bd6b21bc15230031d54bdc2f718c2bb3892bb053
Anton Lindqvist <anton@basename.se>
2019-02-01 18:32:55 +0100

    add another diff root test

--- tests/diff-root.sh
+++ tests/diff-root.sh
@@ -8,6 +8,16 @@ if testcase "simple prefix"; then
 	pass
 fi
 
+if testcase "relative prefix"; then
+	cat <<-EOF >$TMP1
+	Index: kern_descrip.c
+	============================================
+	RCS file: /cvs/src/sys/kern/kern_descrip.c,v
+	EOF
+	assert_eq "/usr/src/sys/kern" "$(diff_root "$TMP1")"
+	pass
+fi
+
 if testcase "complex prefix"; then
 	cat <<-EOF >$TMP1
 	Index: sys/sys/pool.h
