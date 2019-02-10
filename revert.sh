mptre/robsd

commit 3d199048297a2f1809eccb52801e6e36f68e8dba

shellcheck

revert.sh
@@ -5,4 +5,5 @@
 SRCDIR="$(diff_root "$DIFF")"
 [ -z "$SRCDIR" ] && SRCDIR="$BSDSRCDIR"
 (cd "$SRCDIR" && patch -ERs) <"$DIFF"
-find "$BSDSRCDIR" -type f -name '*.orig' -o -name '*.rej' | xargs -rt rm
+find "$BSDSRCDIR" -print0 -type f -name '*.orig' -o -name '*.rej' |
+	xargs -0rt rm

distrib.sh
@@ -1,12 +1,12 @@
 cd "$RELEASEDIR"
 rm -f SHA256
-sha256 -h SHA256 *
+sha256 -h SHA256 -- *
 
 if [ -n "$SIGNIFY" ]; then
 	signify -Se -s "$SIGNIFY" -m SHA256
 fi
 
-ls -nT * >index.txt
+ls -nT -- * >index.txt
 
 su "$SSHUSER" -c "ssh ${SSHHOST} rm -f ${SSHPATH}/*"
 su "$SSHUSER" -c "scp ${RELEASEDIR}/* ${SSHHOST}:${SSHPATH}"

kernel.sh
@@ -3,7 +3,7 @@ kernel_path() {
 
 	_a="$(machine)"
 	_n="$(sysctl -n hw.ncpu)"
-	_s="$(test $_n -gt 1 && echo '.MP')"
+	_s="$(test "$_n" -gt 1 && echo '.MP')"
 
 	printf 'arch/%s/compile/GENERIC%s\n' "$_a" "$_s"
 }

commit f23b3773b480d01427a31c6911cd2b1e069f9e4d

fix cleanup in revert.sh
Regression introduced in commit 3d19904 ("shellcheck").

revert.sh
@@ -5,5 +5,5 @@
 SRCDIR="$(diff_root "$DIFF")"
 [ -z "$SRCDIR" ] && SRCDIR="$BSDSRCDIR"
 (cd "$SRCDIR" && patch -ERs) <"$DIFF"
-find "$BSDSRCDIR" -print0 -type f -name '*.orig' -o -name '*.rej' |
-	xargs -0rt rm
+find "$BSDSRCDIR" -type f \( -name '*.orig' -o -name '*.rej' \) |
+xargs -rt rm
f23b3773b480d01427a31c6911cd2b1e069f9e4d
Anton Lindqvist <anton@basename.se>
2019-02-10 21:22:15 +0100

    fix cleanup in revert.sh
    Regression introduced in commit 3d19904 ("shellcheck").

--- revert.sh
+++ revert.sh
@@ -5,5 +5,5 @@
 SRCDIR="$(diff_root "$DIFF")"
 [ -z "$SRCDIR" ] && SRCDIR="$BSDSRCDIR"
 (cd "$SRCDIR" && patch -ERs) <"$DIFF"
-find "$BSDSRCDIR" -print0 -type f -name '*.orig' -o -name '*.rej' |
-	xargs -0rt rm
+find "$BSDSRCDIR" -type f \( -name '*.orig' -o -name '*.rej' \) |
+xargs -rt rm
