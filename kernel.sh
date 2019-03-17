27540a4b63608b96b04c4238426e82df2436c69c
Anton Lindqvist <anton@basename.se>
2019-03-17 22:04:02 +0100

    simplify kernel stage

--- kernel.sh
+++ kernel.sh
@@ -1,17 +1,14 @@
 kernel_path() {
-	local _a _n _s
+	local _n _s
 
-	_a="$(machine)"
 	_n="$(sysctl -n hw.ncpu)"
 	_s="$(test "$_n" -gt 1 && echo '.MP')"
 
-	printf 'arch/%s/compile/GENERIC%s\n' "$_a" "$_s"
+	printf 'arch/%s/compile/GENERIC%s\n' "$(machine)" "$_s"
 }
 
-KERNEL="$(kernel_path)"
-
-cd "${BSDSRCDIR}/sys"
-make -C "$KERNEL" obj
-make -C "$KERNEL" config
-make -C "$KERNEL"
-make -C "$KERNEL" install
+cd "${BSDSRCDIR}/sys/$(kernel_path)"
+make obj
+make config
+make
+make install
