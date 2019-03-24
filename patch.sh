4d754d40e4ff714d74f64364fb480634ec06c5f0
Anton Lindqvist <anton@basename.se>
2019-03-24 22:20:57 +0100

    add xenocara diff support

--- patch.sh
+++ patch.sh
@@ -1,6 +1,11 @@
 . "${EXECDIR}/util.sh"
 
 if [ -n "$SRCDIFF" ]; then
-	cd "$(diff_root -f "$BSDSRCDIR" "$SRCDIFF")"
+	cd "$(diff_root -f "$BSDSRCDIR" -r src "$SRCDIFF")"
 	patch -Es <"$SRCDIFF"
+fi
+
+if [ -n "$X11DIFF" ]; then
+	cd "$(diff_root -f "$X11SRCDIR" -r xenocara "$X11DIFF")"
+	patch -Es <"$X11DIFF"
 fi
--- revert.sh
+++ revert.sh
@@ -1,8 +1,18 @@
 . "${EXECDIR}/util.sh"
 
-[ -z "$SRCDIFF" ] && exit 0
+diff_clean() {
+	find "$1" -type f \( -name '*.orig' -o -name '*.rej' \) |
+	xargs -rt rm
+}
 
-SRCDIR="$(diff_root -f "$BSDSRCDIR" "$SRCDIFF")"
-(cd "$SRCDIR" && patch -ERs) <"$SRCDIFF"
-find "$BSDSRCDIR" -type f \( -name '*.orig' -o -name '*.rej' \) |
-xargs -rt rm
+if [ -n "$SRCDIFF" ]; then
+	cd "$(diff_root -f "$BSDSRCDIR" -r src "$SRCDIFF")"
+	patch -ERs <"$SRCDIFF"
+	diff_clean "$BSDSRCDIR"
+fi
+
+if [ -n "$X11DIFF" ]; then
+	cd "$(diff_root -f "$X11SRCDIR" -r xenocara "$X11DIFF")"
+	patch -ERs <"$X11DIFF"
+	diff_clean "$X11SRCDIR"
+fi
