2f212d1159e5db6f694ef2e06b57db25031e0d3c
Anton Lindqvist <anton@basename.se>
2019-04-16 12:40:51 +0200

    avoid using NAME which conflicts with t.sh

--- tests/report-size.sh
+++ tests/report-size.sh
@@ -4,10 +4,10 @@ zero() {
 	dd if=/dev/zero "of=${2}" "bs=${1}" count=1 2>/dev/null
 }
 
-NAME="$(basename "$TMP1")"
+FILE="$(basename "$TMP1")"
 
 if testcase "previous release missing"; then
-	assert_eq "${NAME} 0" "$(report_size "$TMP1")"
+	assert_eq "${FILE} 0" "$(report_size "$TMP1")"
 fi
 
 if testcase "previous release missing file"; then
@@ -15,32 +15,32 @@ if testcase "previous release missing file"; then
 	LOGDIR="${BUILDDIR}/2019-02-23"
 	mkdir -p ${BUILDDIR}/2019-02-{22,23}/reldir
 
-	assert_eq "${NAME} 1.0K" "$(report_size "$TMP1")"
+	assert_eq "${FILE} 1.0K" "$(report_size "$TMP1")"
 fi
 
 if testcase "previous delta too small"; then
 	zero 1K "$TMP1"
 	LOGDIR="${BUILDDIR}/2019-02-23"
 	mkdir -p ${BUILDDIR}/2019-02-{22,23}/reldir
-	zero 1K "${BUILDDIR}/2019-02-22/reldir/${NAME}"
+	zero 1K "${BUILDDIR}/2019-02-22/reldir/${FILE}"
 
-	assert_eq "${NAME} 1.0K" "$(report_size "$TMP1")"
+	assert_eq "${FILE} 1.0K" "$(report_size "$TMP1")"
 fi
 
 if testcase "previous delta megabytes"; then
 	zero "$((1024 * 1024 + 438902))" "$TMP1"
 	LOGDIR="${BUILDDIR}/2019-02-23"
 	mkdir -p ${BUILDDIR}/2019-02-{22,23}/reldir
-	zero "$((1024 * 1024))" "${BUILDDIR}/2019-02-22/reldir/${NAME}"
+	zero "$((1024 * 1024))" "${BUILDDIR}/2019-02-22/reldir/${FILE}"
 
-	assert_eq "${NAME} 1.4M (+0.4M)" "$(report_size "$TMP1")"
+	assert_eq "${FILE} 1.4M (+0.4M)" "$(report_size "$TMP1")"
 fi
 
 if testcase "previous delta negative"; then
 	zero 1K "$TMP1"
 	LOGDIR="${BUILDDIR}/2019-02-23"
 	mkdir -p ${BUILDDIR}/2019-02-{22,23}/reldir
-	zero 2M "${BUILDDIR}/2019-02-22/reldir/${NAME}"
+	zero 2M "${BUILDDIR}/2019-02-22/reldir/${FILE}"
 
-	assert_eq "${NAME} 1.0K (-2.0M)" "$(report_size "$TMP1")"
+	assert_eq "${FILE} 1.0K (-2.0M)" "$(report_size "$TMP1")"
 fi
