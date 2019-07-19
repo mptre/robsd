mptre/robsd

commit 9f9f768ad8bb785a490e298431d497f40e7af277

move the report beast to util.sh
* Add tests

* It's no longer necessary to create the stats section last since all data
  is available prior to iterating over all stages

util.sh
@@ -274,6 +274,83 @@ reboot_commence() {
 	shutdown -r '+1' </dev/null >/dev/null 2>&1
 }
 
+# report [-M] -r report -s stages
+#
+# Create a build report and save it to report.
+report() {
+	local _duration=0 _i=1 _mail=1 _name=""
+	local _report _stages _status
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		-M)	_mail=0;;
+		-r)	shift; _report="$1";;
+		-s)	shift; _stages="$1";;
+		*)	break;;
+		esac
+		shift
+	done
+
+	if ! [ -e "$_stages" ]; then
+		echo "report: ${_stages}: no such file"
+		return 1
+	fi
+
+	# Clear or create report. Useful when resuming a build only to re-create
+	# the report.
+	echo -n >"$_report"
+
+	# Add comment to the beginning of the report.
+	if [ -e "${LOGDIR}/comment" ]; then
+		cat <<-EOF >>"$_report"
+		> comment:
+		$(cat "${LOGDIR}/comment")
+
+		EOF
+	fi
+
+	# Add stats to the beginning of the report.
+	stage_eval -1 "$_stages"
+	if [ "$(stage_value exit)" -eq 0 ]; then
+		_status="ok"
+		_duration="$(stage_value duration)"
+		_duration="$(report_duration -d end "$_duration")"
+	else
+		_status="failed in $(stage_value name)"
+		_duration="$(build_duration "$_stages")"
+		_duration="$(report_duration "$_duration")"
+	fi
+	cat <<-EOF >>"$_report"
+	> stats:
+	Build: ${LOGDIR}
+	Status: ${_status}
+	Duration: ${_duration}
+	Size: $(report_size "$(release_dir "$LOGDIR")/bsd")
+	Size: $(report_size "$(release_dir "$LOGDIR")/bsd.mp")
+	Size: $(report_size "$(release_dir "$LOGDIR")/bsd.rd")
+	EOF
+
+	while stage_eval "$_i" "$_stages"; do
+		_i=$((_i + 1))
+
+		_name="$(stage_value name)"
+		report_skip "$_name" && continue
+
+		_duration="$(stage_value duration)"
+
+		printf '\n> %s:\n' "$_name"
+		printf 'Exit: %d\n' "$(stage_value exit)"
+		printf 'Duration: %s\n' "$(report_duration -d "$_name" "$_duration")"
+		printf 'Log: %s\n' "$(basename "$(stage_value log)")"
+		report_log "$_name" "$(stage_value log)"
+	done >>"$_report"
+
+	# Do not send mail during interactive invocations.
+	{ [ -t 0 ] || [ "$_mail" -eq 0 ]; } && return 0
+
+	mail -s "robsd: $(machine): ${_status}" root <"$_report"
+}
+
 # report_duration [-d stage] duration
 #
 # Format the given duration to a human readable representation.
@@ -312,6 +389,27 @@ report_duration() {
 		"$(format_duration "$_d")" \
 		"$_sign" \
 		"$(format_duration "$_delta")"
+}
+
+# report_log stage log
+#
+# Writes an excerpt of the given log.
+report_log() {
+	[ -s "$2" ] && echo
+
+	case "$1" in
+	cvs|patch|revert|distrib)
+		cat "$2"
+		;;
+	checkflist)
+		# Silent if the log only contains PS4 traces.
+		grep -vq '^\+' "$2" || return 0
+		cat "$2"
+		;;
+	*)
+		tail "$2"
+		;;
+	esac
 }
 
 # report_size file

tests/report.sh
@@ -1,0 +1,66 @@
+LOGDIR="${WRKDIR}/logdir"
+STAGES="${WRKDIR}/stages"
+REPORT="${WRKDIR}/report"
+
+if testcase "basic"; then
+	mkdir "$LOGDIR"
+	echo "comment goes here" >"${LOGDIR}/comment"
+	echo "cvs log" >"${LOGDIR}/cvs.log"
+	cat <<-EOF >$STAGES
+	stage="1" name="env" exit="0" duration="0" log="${LOGDIR}/env.log" user="root" time="0"
+	stage="2" name="cvs" exit="0" duration="358" log="${LOGDIR}/cvs.log" user="root" time="0"
+	stage="3" name="end" exit="0" duration="3600" log="" user="root" time="0"
+	EOF
+	cat <<-EOF >$TMP1
+	> comment:
+	comment goes here
+
+	> stats:
+	Build: ${LOGDIR}
+	Status: ok
+	Duration: 01:00:00
+	Size: bsd 0
+	Size: bsd.mp 0
+	Size: bsd.rd 0
+
+	> cvs:
+	Exit: 0
+	Duration: 00:05:58
+	Log: cvs.log
+
+	cvs log
+	EOF
+
+	report -M -r "$REPORT" -s "$STAGES"
+
+	assert_file "$TMP1" "$REPORT"
+fi
+
+if testcase "failure"; then
+	mkdir "$LOGDIR"
+	echo "cvs log" >"${LOGDIR}/cvs.log"
+	cat <<-EOF >$STAGES
+	stage="1" name="env" exit="0" duration="1" log="${LOGDIR}/env.log" user="root" time="0"
+	stage="2" name="cvs" exit="1" duration="10" log="${LOGDIR}/cvs.log" user="root" time="0"
+	EOF
+	cat <<-EOF >$TMP1
+	> stats:
+	Build: ${LOGDIR}
+	Status: failed in cvs
+	Duration: 00:00:11
+	Size: bsd 0
+	Size: bsd.mp 0
+	Size: bsd.rd 0
+
+	> cvs:
+	Exit: 1
+	Duration: 00:00:10
+	Log: cvs.log
+
+	cvs log
+	EOF
+
+	report -M -r "$REPORT" -s "$STAGES"
+
+	assert_file "$TMP1" "$REPORT"
+fi

robsd
@@ -102,108 +102,6 @@ must_report() {
 	esac
 }
 
-# report -r report -s stages
-#
-# Generate a build report and save it to report.
-report() {
-	local _duration=0 _i=1 _name=""
-	local _d _exit _report _stages _status
-
-	while [ $# -gt 0 ]; do
-		case "$1" in
-		-r)	shift; _report="$1";;
-		-s)	shift; _stages="$1";;
-		*)	break;;
-		esac
-		shift
-	done
-
-	[ -e "$_stages" ] || return 0
-
-	while stage_eval "$_i" "$_stages"; do
-		_i=$((_i + 1))
-
-		_name="$(stage_value name)"
-		report_skip "$_name" && continue
-
-		_duration="$(stage_value duration)"
-		_exit="$(stage_value exit)"
-
-		printf '> %s:\n' "$_name"
-		printf 'Exit: %d\n' "$(stage_value exit)"
-		printf 'Duration: %s\n' "$(report_duration -d "$_name" "$_duration")"
-		printf 'Log: %s\n' "$(basename "$(stage_value log)")"
-		report_log "$_name" "$(stage_value log)"
-		printf '\n'
-	done >"$_report"
-
-	_duration="$(build_duration "$_stages")"
-
-	if [ "$_exit" -eq 0 ]; then
-		_status='ok'
-		# Only calculate the build duration delta on success.
-		_d="$(report_duration -d end "$_duration")"
-	else
-		_status="failed in ${_name}"
-		_d="$(report_duration "$_duration")"
-	fi
-
-	# Add stats section to the beginning of the report.
-	ed -s <<-EOF "$_report"
-	H
-	0a
-	> stats:
-	Build: ${LOGDIR}
-	Status: ${_status}
-	Duration: ${_d}
-	Size: $(report_size "$(release_dir "$LOGDIR")/bsd")
-	Size: $(report_size "$(release_dir "$LOGDIR")/bsd.mp")
-	Size: $(report_size "$(release_dir "$LOGDIR")/bsd.rd")
-
-	.
-	w
-	EOF
-
-	# Add comment to the beginning of the report.
-	if [ -e "${LOGDIR}/comment" ]; then
-		ed -s <<-EOF "$_report"
-		H
-		0a
-		> comment:
-		$(cat "${LOGDIR}/comment")
-
-		.
-		w
-		EOF
-	fi
-
-	# Do not send mail during interactive invocations.
-	[ -t 0 ] && return 0
-
-	mail -s "robsd: $(machine): ${_status}" root <"$_report"
-}
-
-# report_log stage log
-#
-# Writes an excerpt of the given log.
-report_log() {
-	[ -s "$2" ] && echo
-
-	case "$1" in
-	cvs|patch|revert|distrib)
-		cat "$2"
-		;;
-	checkflist)
-		# Silent if the log only contains PS4 traces.
-		grep -vq '^\+' "$2" || return 0
-		cat "$2"
-		;;
-	*)
-		tail "$2"
-		;;
-	esac
-}
-
 # stage_begin -l log -n name -s stage
 #
 # Mark the given stage as about to execute by writing an entry to the given

tests/Makefile
@@ -7,6 +7,7 @@ TESTS+=	format-duration.sh
 TESTS+=	purge.sh
 TESTS+=	report-duration.sh
 TESTS+=	report-size.sh
+TESTS+=	report.sh
 TESTS+=	stage-eval.sh
 TESTS+=	stage-next.sh
 

commit baafe523ce727cddf8ccb12330b7b87496133b9c

fix report regression when the stages file is absent
Missing stages file must be allowed as it could happen while starting a new
build which fails due to another already running build.

Regression introduced in commit 9f9f768 ("move the report beast to util.sh").

util.sh
@@ -297,10 +297,9 @@ report() {
 		shift
 	done
 
-	if ! [ -e "$_stages" ]; then
-		echo "report: ${_stages}: no such file"
-		return 1
-	fi
+	# The stages file could be absent when a build fails to start due to
+	# another already running build.
+	[ -e "$_stages" ] || return 0
 
 	# Clear or create report. Useful when resuming a build only to re-create
 	# the report.

tests/report.sh
@@ -64,3 +64,7 @@ if testcase "failure"; then
 
 	assert_file "$TMP1" "$REPORT"
 fi
+
+if testcase "missing stages"; then
+	report -M -r "$REPORT" -s "$STAGES"
+fi