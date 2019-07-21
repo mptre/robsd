974d8890d30791d69ecbf18058dc89713ccdb677
Anton Lindqvist <anton@basename.se>
2019-07-21 22:34:46 +0200

    fail early if patch does not apply
    ... without leaving the repository in a broken state.

--- patch.sh
+++ patch.sh
@@ -2,10 +2,12 @@
 
 if [ -n "$SRCDIFF" ]; then
 	cd "$(diff_root -f "$BSDSRCDIR" -r src "$SRCDIFF")"
+	su "$CVSUSER" -c "patch -Cs" <"$SRCDIFF"
 	su "$CVSUSER" -c "patch -Es" <"$SRCDIFF"
 fi
 
 if [ -n "$X11DIFF" ]; then
 	cd "$(diff_root -f "$X11SRCDIR" -r xenocara "$X11DIFF")"
+	su "$CVSUSER" -c "patch -Cs" <"$X11DIFF"
 	su "$CVSUSER" -c "patch -Es" <"$X11DIFF"
 fi
