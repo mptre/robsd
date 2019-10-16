7eef1c7ab3189b472ee64218dcfe419b7a5f2ac1
Anton Lindqvist <anton@basename.se>
2019-10-16 16:54:12 +0200

    print the command from rc.firsttime

--- util.sh
+++ util.sh
@@ -426,6 +426,7 @@ reboot_commence() {
 	# Do not inherit standard file descriptors in order to let the boot
 	# process proceed.
 	cat <<-EOF >>/etc/rc.firsttime
+	echo /usr/local/sbin/robsd -r ${LOGDIR}
 	(
 	exec </dev/null >/dev/null 2>&1
 	/usr/local/sbin/robsd -r ${LOGDIR} &
