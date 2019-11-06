061425be858c88caeda58146327a29a4fe208e15
Anton Lindqvist <anton@basename.se>
2019-11-06 10:12:40 +0100

    add force flag to patch(1) when checking diff
    Will cause the patch step to fail when a tty is attached to the robsd process.

--- patch.sh
+++ patch.sh
@@ -2,12 +2,12 @@
 
 if [ -n "$SRCDIFF" ]; then
 	cd "$(diff_root -d "$BSDSRCDIR" "$SRCDIFF")"
-	su "$CVSUSER" -c "patch -Cs" <"$SRCDIFF"
+	su "$CVSUSER" -c "patch -Cfs" <"$SRCDIFF"
 	su "$CVSUSER" -c "patch -Es" <"$SRCDIFF"
 fi
 
 if [ -n "$XDIFF" ]; then
 	cd "$(diff_root -d "$XSRCDIR" "$XDIFF")"
-	su "$CVSUSER" -c "patch -Cs" <"$XDIFF"
+	su "$CVSUSER" -c "patch -Cfs" <"$XDIFF"
 	su "$CVSUSER" -c "patch -Es" <"$XDIFF"
 fi
