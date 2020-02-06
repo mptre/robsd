mptre/robsd

commit 8f61ebd436d45fc36e22c296cc50f7ff43fb1eb4

fix send report conditional
While running robsd from cron in non-detach mode it must send the report
in case of failure.

robsd
@@ -22,7 +22,7 @@ atexit() {
 	if [ "$_err" -ne 0 ] || must_report "$STEP"; then
 		report -r "${LOGDIR}/report" -s "${LOGDIR}/steps"
 		# Do not send mail during interactive invocations.
-		if [ "$DETACH" -eq 1 ]; then
+		if ! [ -t 0 ]; then
 			sendmail root <"${LOGDIR}/report"
 		fi
 	fi

commit 9abd8849f4c259800160ea0e3c576a87b43797e2

fix regression report send logic
This logic is tricky to get right. If we fail to run due to robsd
already running, the report will be absent. Therefore make report return
non-zero under such circumstances.

Regression introduced in commit 8f61ebd ("fix send report conditional").

robsd
@@ -20,10 +20,11 @@ atexit() {
 	lock_release "$BUILDDIR"
 
 	if [ "$_err" -ne 0 ] || must_report "$STEP"; then
-		report -r "${LOGDIR}/report" -s "${LOGDIR}/steps"
-		# Do not send mail during interactive invocations.
-		if ! [ -t 0 ]; then
-			sendmail root <"${LOGDIR}/report"
+		if report -r "${LOGDIR}/report" -s "${LOGDIR}/steps"; then
+			# Do not send mail during interactive invocations.
+			if ! [ -t 0 ]; then
+				sendmail root <"${LOGDIR}/report"
+			fi
 		fi
 	fi
 

util.sh
@@ -562,7 +562,7 @@ report() {
 
 	# The steps file could be absent when a build fails to start due to
 	# another already running build.
-	[ -e "$_steps" ] || return 0
+	[ -e "$_steps" ] || return 1
 
 	_tmp="$(mktemp -t robsd.XXXXXX)"
 

tests/report.sh
@@ -117,5 +117,7 @@ if testcase "failure in skipped step"; then
 fi
 
 if testcase "missing step"; then
-	report -r "$REPORT" -s "$STEPS"
+	if report -r "$REPORT" -s "$STEPS"; then
+		fail "want exit 1, got 0"
+	fi
 fi