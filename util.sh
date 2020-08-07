mptre/robsd

commit 8243e2f3ad87054cc0f0d32c2f7ed9b56f126c29

add integration test
Runs robsd but will all stages stubbed out.

util.sh
@@ -73,7 +73,6 @@ config_load() {
 	export KEEP; : "${KEEP:=0}"
 	export LOGDIR
 	export MAKEFLAGS; : "${MAKEFLAGS:="-j$(sysctl -n hw.ncpuonline)"}"
-	export PATH; PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin"
 	export RELEASEDIR
 	export SIGNIFY; : "${SIGNIFY:=}"
 	export SKIP; : "${SKIP:=}"

robsd
@@ -195,7 +195,7 @@ step_exec() (
 
 setprogname "robsd"
 [ "$(id -u)" -ne 0 ] && fatal "must be run as root"
-config_load "/etc/robsdrc"
+config_load "${ROBSDRC:-/etc/robsdrc}"
 
 DETACH=0
 LOGDIR=""

robsd-clean
@@ -13,7 +13,7 @@ usage() {
 
 setprogname "robsd-clean"
 [ "$(id -u)" -ne 0 ] && fatal "must be run as root"
-config_load "/etc/robsdrc"
+config_load "${ROBSDRC:-/etc/robsdrc}"
 
 purge "$BUILDDIR" "$1" | while read -r _d; do
 	info "moving ${_d} to the attic"

tests/Makefile
@@ -16,6 +16,7 @@ TESTS+=	report-duration.sh
 TESTS+=	report-size.sh
 TESTS+=	report-skip.sh
 TESTS+=	report.sh
+TESTS+=	robsd.sh
 TESTS+=	step-end.sh
 TESTS+=	step-eval.sh
 TESTS+=	step-id.sh

tests/robsd.sh
@@ -1,0 +1,60 @@
+WRKDIR="$(mktemp -d -t robsd.XXXXXX)"
+TSHCLEAN="${TSHCLEAN} ${WRKDIR}"
+
+BUILDDIR="${TSHDIR}/build"
+ROBSDRC="${WRKDIR}/robsdrc"; export ROBSDRC
+PATH="${WRKDIR}/bin:${PATH}"
+ROBSD="${EXECDIR}/robsd"
+
+# Default configuration.
+cat <<-EOF >"$ROBSDRC"
+CVSROOT=example.com:/cvs
+CVSUSER=nobody
+DESTDIR=/var/empty
+DISTRIBHOST=example.com
+DISTRIBPATH=/var/empty
+DISTRIBUSER=nobody
+EOF
+
+# Stub utilities.
+mkdir -p "${WRKDIR}/bin"
+
+cat <<EOF >"${WRKDIR}/bin/id"
+echo 0
+EOF
+chmod u+x "${WRKDIR}/bin/id"
+
+cat <<EOF >"${WRKDIR}/bin/sysctl"
+if [ "\$2" = "hw.perfpolicy" ]; then
+	echo auto
+else
+	/usr/sbin/sysctl "$@"
+fi
+EOF
+chmod u+x "${WRKDIR}/bin/sysctl"
+
+# Create exec directory including all stages.
+mkdir -p "${WRKDIR}/exec"
+cp "${EXECDIR}/util.sh" "${WRKDIR}/exec"
+for _stage in \
+	env \
+        cvs \
+        patch \
+        kernel \
+        reboot \
+        base \
+        release \
+        checkflist \
+        xbase \
+        xrelease \
+        image \
+        revert \
+        distrib
+do
+	: >"${WRKDIR}/exec/${_stage}.sh"
+done
+
+if testcase "basic"; then
+	mkdir -p "$BUILDDIR"
+	EXECDIR="${WRKDIR}/exec" sh "$ROBSD" >/dev/null 2>&1
+fi

commit 503261565793eb254990f0406366ef6fe5927c32

add x11 path
Regression introduced in commit ("8243e2f add integration test"), neede while
building xenocara.

util.sh
@@ -73,6 +73,7 @@ config_load() {
 	export KEEP; : "${KEEP:=0}"
 	export LOGDIR
 	export MAKEFLAGS; : "${MAKEFLAGS:="-j$(sysctl -n hw.ncpuonline)"}"
+	export PATH; : "${PATH}:/usr/X11R6/bin"
 	export RELEASEDIR
 	export SIGNIFY; : "${SIGNIFY:=}"
 	export SKIP; : "${SKIP:=}"