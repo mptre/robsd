d7fc6dbe185bcee307c06b5b29487b2e77f2623a
Anton Lindqvist <anton@basename.se>
2020-09-16 11:03:25 +0200

    only release the lock if we did acquire it
    Running robsd twice while another build is already running would cause
    the second invocation to continue since the lock was unconditionally
    removed.

--- tests/robsd.sh
+++ tests/robsd.sh
@@ -51,5 +51,38 @@ done
 if testcase "basic"; then
 	config_stub
 	mkdir -p "$BUILDDIR"
-	EXECDIR="${WRKDIR}/exec" sh "$ROBSD" >/dev/null 2>&1
+	EXECDIR="${WRKDIR}/exec" sh "$ROBSD" >"$TMP1" 2>&1
+	if [ -e "${BUILDDIR}/.running" ]; then
+		fail - "lock not removed" <"$TMP1"
+	fi
+fi
+
+if testcase "already running"; then
+	config_stub
+	mkdir -p "$BUILDDIR"
+	echo /var/empty >"${BUILDDIR}/.running"
+	EXECDIR="${WRKDIR}/exec" sh "$ROBSD" 2>&1 | grep -v 'using ' >"$TMP1"
+	if ! [ -e "${BUILDDIR}/.running" ]; then
+		fail - "lock not preserved" <"$TMP1"
+	fi
+	assert_file - "$TMP1" <<-EOF
+	robsd: /var/empty: lock already acquired
+	robsd: already running
+	robsd: failed in step unknown
+	EOF
+fi
+
+if testcase "already running detached"; then
+	config_stub
+	mkdir -p "$BUILDDIR"
+	echo /var/empty >"${BUILDDIR}/.running"
+	EXECDIR="${WRKDIR}/exec" sh "$ROBSD" -D 2>&1 | grep -v 'using ' >"$TMP1"
+	if ! [ -e "${BUILDDIR}/.running" ]; then
+		fail - "lock not preserved" <"$TMP1"
+	fi
+	assert_file - "$TMP1" <<-EOF
+	robsd: /var/empty: lock already acquired
+	robsd: already running
+	robsd: failed in step unknown
+	EOF
 fi
