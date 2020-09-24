mptre/robsd

commit 7ccd911d062de415fcbeb7e6c2dd57fb30410339

rework handling of release directories
Split src and xenocara release directories in order to fix the SHA256
shipped with installXX.iso. This also stops recomputing the SHA over
again, it's only performed for the files missing a SHA.

This is a breaking change as reldir is renamed to rel and the introduced
xenocara directory relx. The names are taken from
distrib/amd64/iso/Makefile.

release.sh
@@ -1,18 +1,18 @@
 . "${EXECDIR}/util.sh"
 
+DESTDIR="${DESTDIR}/src"
+mkdir -p "$DESTDIR"
 cleandir "$DESTDIR"
 chown build "$DESTDIR"
 chmod 700 "$DESTDIR"
 
-DESTDIR="${DESTDIR}/src"
-mkdir -p "$DESTDIR"
-
+RELEASEDIR="$(release_dir "$LOGDIR")"; export RELEASEDIR
 mkdir -p "$RELEASEDIR"
+cleandir "$RELEASEDIR"
 chown build "$RELEASEDIR"
 chmod 755 "$RELEASEDIR"
 
-# In case of resuming, wipe the release directory and remove vnd devices.
-cleandir "$RELEASEDIR"
+# Remove vnd devices in case of resuming.
 make -C "${BSDSRCDIR}/distrib/$(machine)" unconfig
 
 cd "${BSDSRCDIR}/etc"

Makefile
@@ -3,6 +3,7 @@ SCRIPTS+=	checkflist.sh
 SCRIPTS+=	cvs.sh
 SCRIPTS+=	distrib.sh
 SCRIPTS+=	env.sh
+SCRIPTS+=	hash.sh
 SCRIPTS+=	image.sh
 SCRIPTS+=	kernel.sh
 SCRIPTS+=	patch.sh

base.sh
@@ -1,4 +1,4 @@
-unset DESTDIR RELEASEDIR
+unset DESTDIR
 
 chown build:wobj "$BSDOBJDIR"
 chmod 770 "$BSDOBJDIR"

distrib.sh
@@ -1,6 +1,9 @@
+. "${EXECDIR}/util.sh"
+
+# At this point, all release artifacts are present in the rel directory as the
+# hash step merges the relx directory into rel.
+RELEASEDIR="$(release_dir "$LOGDIR")"
 cd "$RELEASEDIR"
-rm -f index.txt SHA256{,.sig}
-sha256 -h SHA256 -- *
 
 if [ -n "$SIGNIFY" ]; then
 	signify -Se -s "$SIGNIFY" -m SHA256

hash.sh
@@ -1,0 +1,20 @@
+. "${EXECDIR}/util.sh"
+
+RELDIR="$(release_dir "$LOGDIR")"
+RELXDIR="$(release_dir -x "$LOGDIR")"
+
+if [ -e "${RELXDIR}/SHA256" ]; then
+	cat "${RELXDIR}/SHA256" >>"${RELDIR}/SHA256"
+	rm "${RELXDIR}/SHA256"
+fi
+
+if [ -d "$RELXDIR" ]; then
+	find "$RELXDIR" -type f -exec mv {} "$RELDIR" \;
+	rm -r "$RELXDIR"
+fi
+
+# Compute missing SHA for install*.{img,iso}.
+cd "$RELDIR"
+mv SHA256 SHA256.orig
+{ grep -v 'install.*' SHA256.orig; sha256 install*; } | sort >SHA256
+rm SHA256.orig

image.sh
@@ -1,8 +1,8 @@
-DESTDIR="${DESTDIR}/src"
-[ -e "$DESTDIR" ] || exit 1
+. "${EXECDIR}/util.sh"
 
-RELDIR="$RELEASEDIR"; export RELDIR
-RELXDIR="$RELEASEDIR"; export RELXDIR
+DESTDIR="${DESTDIR}/src"
+RELDIR="$(release_dir "$LOGDIR")"; export RELDIR
+RELXDIR="$(release_dir -x "$LOGDIR")"; export RELXDIR
 
 cd "${BSDSRCDIR}/distrib/$(machine)/iso"
 make

robsd
@@ -232,10 +232,6 @@ fi
 touch "${LOGDIR}/robsd.log"
 info "using directory ${LOGDIR} at step ${S}"
 
-# Redundant export since it's already done by config_load but
-# silence shellcheck.
-RELEASEDIR="$(release_dir "$LOGDIR")"; export RELEASEDIR
-
 if ! lock_acquire "$BUILDDIR" "$LOGDIR"; then
 	# Signal to info() that no further output should be written to robsd.log
 	# as we're about the remove the directory where the same log resides.

robsd-clean.8
@@ -26,7 +26,7 @@ Only the following files are kept in order to reduce disk usage:
 .Pa comment ,
 .Pa cvs.log
 .Pa env.log
-.Pa reldir/index.txt ,
+.Pa rel/index.txt ,
 .Pa report ,
 .Pa src.diff ,
 .Pa steps

commit 618abf339b28c845bb61c4fc95eb5bf1a55cf1de

correct destdir owner and permissions
Even though the DESTDIR for the base system resides in a subdirectory on
a file system mounted with noperm, the actual mount point must be owned
by build and have permissions 700. This is even stated in the release(8)
manual.

Regression introduced in commit 7ccd911 ("rework handling of release
directories").

release.sh
@@ -1,5 +1,9 @@
 . "${EXECDIR}/util.sh"
 
+# Required by $BSDSRCDIR/etc/Makefile.
+chown build "$DESTDIR"
+chmod 700 "$DESTDIR"
+
 DESTDIR="${DESTDIR}/src"
 mkdir -p "$DESTDIR"
 cleandir "$DESTDIR"