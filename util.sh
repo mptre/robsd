mptre/robsd

commit 21748c282bfe37f0531c9e75d8ff6549c31a0dc8

change report mail conditional to use detach

util.sh
@@ -1585,7 +1585,7 @@ trap_exit() {
 	if [ "$_err" -ne 0 ] || report_must "$_STEPNAME"; then
 		if report -r "${_logdir}/report" -s "${_logdir}/steps"; then
 			# Do not send mail during interactive invocations.
-			if ! [ -t 0 ]; then
+			if [ "$DETACH" -eq 1 ]; then
 				sendmail root <"${_logdir}/report"
 			fi
 		fi

robsd-regress.8
@@ -26,13 +26,14 @@ In addition, each regression test has a corresponding log file in the same
 directory.
 .Bl -tag -width Ds
 .It Pa report
-A summary of the run which is delivered as a mail to root once
+A summary of the run.
+Delivered as a mail to root while running in detached mode once
 .Nm
 exits.
 .It Pa robsd.log
 Standard output and error from
 .Nm
-when running in detached mode.
+while running in detached mode.
 .It Pa steps
 Internal representation of the executed regression tests.
 This file should never be edited manually.

robsd.8
@@ -71,13 +71,14 @@ Directory containing the final release files.
 Referred to as RELEASEDIR in
 .Xr release 8 .
 .It Pa report
-A summary of the release build which is delivered as a mail to root once
+A summary of the release build.
+Delivered as a mail to root while running in detached mode once
 .Nm
 exits.
 .It Pa robsd.log
 Standard output and error from
 .Nm
-when running in detached mode.
+while running in detached mode.
 .It Pa steps
 Internal representation of the executed steps.
 This file should never be edited manually.

commit 35516094c03cac88cd1c9ddfd094c30fd1b0b1b1

correct sendmail conditional
DETACH can be greater than one once we're fully detached.

Regression introduced in commit 21748c2 ("change report mail conditional to use
detach").

util.sh
@@ -1620,7 +1620,7 @@ trap_exit() {
 	if [ "$_err" -ne 0 ] || report_must "$_STEPNAME"; then
 		if report -r "${_logdir}/report" -s "${_logdir}/steps"; then
 			# Do not send mail during interactive invocations.
-			if [ "$DETACH" -eq 1 ]; then
+			if [ "$DETACH" -ne 0 ]; then
 				sendmail root <"${_logdir}/report"
 			fi
 		fi