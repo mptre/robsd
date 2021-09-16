cc6f74335919a8a4160a7533373b2f1a803de217
Anton Lindqvist <anton@basename.se>
2021-09-16 10:57:04 +0200

    unset destdir in robsd-regress
    Otherwise, some tests might pick up compiler flags in bsd.sys.mk.

--- robsd-regress-exec.sh
+++ robsd-regress-exec.sh
@@ -1,4 +1,6 @@
 . "${EXECDIR}/util.sh"
 
+# Prevent picking up compiler flags in bsd.sys.mk.
+unset DESTDIR
 regress_parallel "$1" || unset MAKEFLAGS
 unpriv "$REGRESSUSER" "exec make -C ${BSDSRCDIR}/regress/${1}"
