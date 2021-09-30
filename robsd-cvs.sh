13aef9776bbeb6fd7651d1f8ff0ab4e6e6615806
Anton Lindqvist <anton@basename.se>
2021-09-30 07:14:58 +0200

    minor nits in robsd-cvs.sh

--- robsd-cvs.sh
+++ robsd-cvs.sh
@@ -1,11 +1,10 @@
 . "${EXECDIR}/util.sh"
 
-for d in "$BSDSRCDIR" "$XSRCDIR"; do
-	_tmpdir="${BUILDDIR}/tmp"
+_tmpdir="${BUILDDIR}/tmp"
 
-	unpriv "$CVSUSER" "cd ${d} && exec cvs -q -d ${CVSROOT} update -Pd" 2>&1 |
+for _d in "$BSDSRCDIR" "$XSRCDIR"; do
+	unpriv "$CVSUSER" "cd ${_d} && exec cvs -q -d ${CVSROOT} update -Pd" 2>&1 |
 		tee "${_tmpdir}/cvs.log"
-	find "$d" -type f -name Root -delete
-
-	cvs_log -r "$d" -t "$_tmpdir" -u "$CVSUSER" <"${_tmpdir}/cvs.log"
+	find "$_d" -type f -name Root -delete
+	cvs_log -r "$_d" -t "$_tmpdir" -u "$CVSUSER" <"${_tmpdir}/cvs.log"
 done
