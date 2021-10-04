db3e226a61e479251691719619cd50095d3b04ba
Anton Lindqvist <anton@basename.se>
2021-10-04 07:42:32 +0200

    favor make subdir instead of handling mystuff

--- robsd-ports-outdated.sh
+++ robsd-ports-outdated.sh
@@ -2,28 +2,19 @@
 
 chroot "$CHROOT" sh -ex <<'EOF' >"${BUILDDIR}/tmp/outdated.log"
 cd $PORTSDIR
+
 _DEPENDS_CACHE=$(make create_DEPENDS_CACHE); export _DEPENDS_CACHE
 
-_arch=$(machine)
-
 for _p in $PORTS; do
-	cd ${PORTSDIR}/${_p} 2>/dev/null || cd ${PORTSDIR}/mystuff/${_p}
-
-	_pkgfile="$(make show=PKGFILE)"
+	_pkgfile="$(env "SUBDIR=${_p}" make show=PKGFILE | grep -v '^===> ')"
 	if [ -e $_pkgfile ]; then
 		_s1=$(pkg_info -S -Dunsigned $_pkgfile 2>&1 | sed -n -e 's/Signature: //p')
-		_s2=$(make print-update-signature)
+		_s2="$(env "SUBDIR=${_p}" make print-update-signature | grep -v '^===> ')"
 		[ "$_s1" = "$_s2" ] && continue
 	fi
-
-	# Capture the plist, later used to generate a diff.
-	_pkgname=$(make show=FULLPKGNAME)
-	_plist=$(make show=PLIST_REPOSITORY)/${_arch}/${_pkgname}
-	cat  ${_plist} >/tmp/${_pkgname}.plist || :
 
 	echo $_p
 done
 
-cd $PORTSDIR
 make destroy_DEPENDS_CACHE
 EOF
