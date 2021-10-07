mptre/robsd

commit 2539a5ee15b0d00860eccfce9c9a1028829051c2

remove last global local

util.sh
@@ -1025,20 +1025,6 @@ report_log() {
 	esac
 }
 
-# report_must step-name
-#
-# Exits 0 if a report must be generated.
-report_must() {
-	local _name
-
-	_name="$1"; : "${_name:?}"
-
-	case "$_name" in
-	end)	return 0;;
-	*)	return 1;;
-	esac
-}
-
 # report_size file
 #
 # If the given file is significantly larger than the same file in the previous
@@ -1166,6 +1152,7 @@ release_dir() {
 robsd() {
 	local _exit
 	local _log
+	local _name
 	local _s
 	local _step
 	local _t0
@@ -1174,41 +1161,41 @@ robsd() {
 	_step="$1"; : "${_step:?}"
 
 	while :; do
-		_STEPNAME="$(step_name "$_step")"
+		_name="$(step_name "$_step")"
 		_s="$_step"
 		_step=$((_step + 1))
 
-		if step_eval -n "$_STEPNAME" "${BUILDDIR}/steps" 2>/dev/null && step_skip; then
-			info "step ${_STEPNAME} skipped"
+		if step_eval -n "$_name" "${BUILDDIR}/steps" 2>/dev/null && step_skip; then
+			info "step ${_name} skipped"
 			continue
 		else
-			info "step ${_STEPNAME}"
+			info "step ${_name}"
 		fi
 
-		if [ "$_STEPNAME" = "end" ]; then
+		if [ "$_name" = "end" ]; then
 			# The duration of the end step is the accumulated
 			# duration.
 			step_end -d "$(duration_total "${BUILDDIR}/steps")" \
-				-n "$_STEPNAME" -s "$_s" "${BUILDDIR}/steps"
+				-n "$_name" -s "$_s" "${BUILDDIR}/steps"
 			return 0
 		fi
 
-		_log="${BUILDDIR}/$(log_id -b "$BUILDDIR" -n "$_STEPNAME" -s "$_s")"
+		_log="${BUILDDIR}/$(log_id -b "$BUILDDIR" -n "$_name" -s "$_s")"
 		_exit=0
 		_t0="$(date '+%s')"
-		step_begin -l "$_log" -n "$_STEPNAME" -s "$_s" "${BUILDDIR}/steps"
-		step_exec -f "${BUILDDIR}/fail" -l "$_log" -s "$_STEPNAME" || \
+		step_begin -l "$_log" -n "$_name" -s "$_s" "${BUILDDIR}/steps"
+		step_exec -f "${BUILDDIR}/fail" -l "$_log" -s "$_name" || \
 			_exit="$?"
 		_t1="$(date '+%s')"
 		step_end -d "$((_t1 - _t0))" -e "$_exit" -l "$_log" \
-			-n "$_STEPNAME" -s "$_s" "${BUILDDIR}/steps"
+			-n "$_name" -s "$_s" "${BUILDDIR}/steps"
 
 		case "$_MODE" in
 		robsd-ports)
-			ports_continue -e "$_exit" -n "$_STEPNAME" || return 1
+			ports_continue -e "$_exit" -n "$_name" || return 1
 			;;
 		robsd-regress)
-			regress_continue -e "$_exit" -n "$_STEPNAME" || return 1
+			regress_continue -e "$_exit" -n "$_name" || return 1
 			;;
 		*)
 			[ "$_exit" -eq 0 ] || return 1
@@ -1216,7 +1203,7 @@ robsd() {
 		esac
 
 		# Reboot in progress?
-		[ "$_STEPNAME" = "reboot" ] && return 0
+		[ "$_name" = "reboot" ] && return 0
 	done
 }
 
@@ -1619,7 +1606,7 @@ trap_exit() {
 	local _builddir=""
 	local _rootdir=""
 
-	info "trap at step ${_STEPNAME}, exit ${_err}"
+	info "trap exit ${_err}"
 
 	while [ $# -gt 0 ]; do
 		case "$1" in
@@ -1635,12 +1622,10 @@ trap_exit() {
 
 	lock_release "$_rootdir" "$_builddir" || :
 
-	if [ "$_err" -ne 0 ] || report_must "$_STEPNAME"; then
-		if report -b "$_builddir"; then
-			# Do not send mail during interactive invocations.
-			if [ "$DETACH" -ne 0 ]; then
-				sendmail root <"${_builddir}/report"
-			fi
+	if report -b "$_builddir"; then
+		# Do not send mail during interactive invocations.
+		if [ "$DETACH" -ne 0 ]; then
+			sendmail root <"${_builddir}/report"
 		fi
 	fi
 
@@ -1665,7 +1650,3 @@ unpriv() (
 	USER="$_user"
 	su "$_user" -c "$@"
 )
-
-# Global locals only used in this file. Since this file is source by step
-# scripts, preserve any existing value.
-: "${_STEPNAME:="unknown"}"

tests/robsd.sh
@@ -96,7 +96,7 @@ if testcase "basic"; then
 	robsd: invoking hook: ${_hook} ${_builddir} end 0
 	stdout
 	stderr
-	robsd: trap at step end, exit 0
+	robsd: trap exit 0
 	EOF
 fi
 
@@ -112,7 +112,7 @@ if testcase "already running"; then
 	fi
 	assert_file - "$TMP1" <<-EOF
 	robsd: /var/empty: lock already acquired
-	robsd: trap at step unknown, exit 1
+	robsd: trap exit 1
 	EOF
 fi
 
@@ -128,7 +128,7 @@ if testcase "already running detached"; then
 	fi
 	assert_file - "$TMP1" <<-EOF
 	robsd: /var/empty: lock already acquired
-	robsd: trap at step unknown, exit 1
+	robsd: trap exit 1
 	EOF
 fi
 
@@ -143,7 +143,7 @@ if testcase "early failure"; then
 	fi
 	assert_file - "$TMP1" <<-EOF
 	robsd: non-optimal performance detected, check hw.perfpolicy and hw.setperf
-	robsd: trap at step unknown, exit 1
+	robsd: trap exit 1
 	EOF
 fi
 

commit 2a06796fe5d5110c2e6a5a7fccb323b7734664fc

fix report regression
Do not generate and mail out report while running in detached mode and reaching
the reboot step. Regression introduced in commit 2539a5e ("remove last global
local").

util.sh
@@ -1621,9 +1621,12 @@ trap_exit() {
 
 	lock_release "$_rootdir" "$_builddir" || :
 
-	if report -b "$_builddir"; then
-		# Do not send mail during interactive invocations.
-		if [ "$DETACH" -ne 0 ]; then
+	# Generate the report if a step failed or the end step is reached.
+	if [ "$_err" -ne 0 ] ||
+	   step_eval -n end "${_builddir}/steps" 2>/dev/null
+	then
+		if report -b "$_builddir" && [ "$DETACH" -ne 0 ]; then
+			# Do not send mail during interactive invocations.
 			sendmail root <"${_builddir}/report"
 		fi
 	fi

tests/robsd.sh
@@ -100,6 +100,25 @@ if testcase "basic"; then
 	EOF
 fi
 
+if testcase "reboot"; then
+	config_stub - <<-EOF
+	ROBSDDIR=${ROBSDDIR}
+	EXECDIR=${WRKDIR}/exec
+	EOF
+	mkdir -p "$ROBSDDIR"
+
+	_fail="${TSHDIR}/fail"
+	PATH="${BINDIR}:${PATH}" sh "$ROBSD" >"$TMP1" 2>&1 || : >"$_fail"
+	if [ -e "$_fail" ]; then
+		fail - "expected exit zero" <"$TMP1"
+	fi
+
+	_builddir="${ROBSDDIR}/$(date '+%Y-%m-%d').1"
+	if [ -e "${_builddir}/report" ]; then
+		fail - "expected no report" <"$TMP1"
+	fi
+fi
+
 if testcase "already running"; then
 	config_stub - <<-EOF
 	ROBSDDIR=${ROBSDDIR}