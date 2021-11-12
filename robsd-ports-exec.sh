2fcf179045025c8743d43114bde5ea9754e2a4b3
Anton Lindqvist <anton@basename.se>
2021-11-12 11:11:39 +0100

    sign all packages, including debug ones

--- robsd-ports-exec.sh
+++ robsd-ports-exec.sh
@@ -11,6 +11,9 @@ EOF
 
 [ -n "$SIGNIFY" ] || exit 0
 
-_pkgfile="$(chroot "$CHROOT" env "SUBDIR=${1}" make -C "$PORTSDIR" show=PKGFILE | grep -v '^===> ')"
-cd "${CHROOT}${_pkgfile%/*}"
-pkg_sign -s signify2 -s "$SIGNIFY" "${_pkgfile##*/}"
+# By default pkg_sign(1) writes out the signed package to the current directory,
+# hence the cd.
+chroot "$CHROOT" env "SUBDIR=${1}" make -C "$PORTSDIR" show=PKGFILES |
+grep -v '^===> ' |
+xargs printf "${CHROOT}%s\n" |
+xargs -t -I{} sh -c "cd {}/.. && pkg_sign -s signify2 -s ${SIGNIFY} {}"
