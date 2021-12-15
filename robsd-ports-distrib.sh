mptre/robsd

commit 87f8532591318ed6581654f3ab9ae2f1344a012f

make use of dpb under the hood in robsd-ports
Writing robsd-ports was a good exercise but detecting outdated dependencies was
harder than expected. Better of making use of what we got instead.

robsd-ports
@@ -63,6 +63,12 @@ if [ "$STEP" -eq 1 ]; then
 			_id="$(step_id "$_step")"
 			step_end -S -n "$_step" -s "$_id" "${BUILDDIR}/steps"
 		done
+		# Flag all ports as skipped initially, later populated by the
+		# dpb step.
+		for _p in $PORTS; do
+			_id="$(step_id "$_p")"
+			step_end -S -n "$_p" -s "$_id" "${BUILDDIR}/steps"
+		done
 	fi
 
 	if [ -n "$TAGS" ]; then

util-ports.sh
@@ -35,14 +35,47 @@ ports_config_load() {
 	unset PKG_PATH
 }
 
-# ports_parallel port
+# ports_duration_total -s steps
 #
-# Exits zero if the given port can build in parallel.
-ports_parallel() {
-	local _port
+# Get the total duration.
+ports_duration_total() {
+	local _d
+	local _i=1
+	local _name
+	local _steps
+	local _tot=0
 
-	_port="$1"; : "${_port:?}"
-	! echo "$NOPARALLEL" | grep -q "\<${_port}\>"
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		-s)	shift; _steps="$1";;
+		*)	break;;
+		esac
+		shift
+	done
+	: "${_steps:?}"
+
+	while step_eval "$_i" "$_steps" 2>/dev/null; do
+		_i=$((_i + 1))
+
+		step_skip && continue
+
+		_name="$(step_value name)"
+
+		# Do not include the previous accumulated build duration.
+		# Could be present if the report is re-generated.
+		[ "$_name" = "end" ] && continue
+
+		# Ports are already covered by the total dpb duration.
+		case "$PORTS" in
+		*${_name}*)	continue;;
+		*)		;;
+		esac
+
+		_d="$(step_value duration)"
+		_tot=$((_tot + _d))
+	done
+
+	echo "$_tot"
 }
 
 # ports_report_log -e step-exit -n step-name -l step-log -t tmp-dir
@@ -108,7 +141,7 @@ ports_report_skip() {
 	: "${_name:?}"
 
 	case "$_name" in
-	env|proot|outdated|distrib|end)
+	env|proot|dpb|distrib|end)
 		return 0
 		;;
 	cvs)
@@ -120,72 +153,68 @@ ports_report_skip() {
 	esac
 }
 
-# ports_step_after -b build-dir -e step-exit -n step-name
+# ports_report_status -s steps
 #
-# After step hook, exits 0 if we can continue.
-ports_step_after() {
-	local _builddir
-	local _exit
+# Get the report status subject.
+ports_report_status() {
 	local _n
-	local _name
-	local _outdated
-	local _s
 	local _steps
 
 	while [ $# -gt 0 ]; do
 		case "$1" in
-		-b)	shift; _builddir="$1";;
-		-e)	shift; _exit="$1";;
-		-n)	shift; _name="$1";;
+		-s)	shift; _steps="$1";;
 		*)	break;;
 		esac
 		shift
 	done
-	: "${_builddir:?}"
-	: "${_exit:?}"
-	: "${_name:?}"
+	: "${_steps:?}"
 
-	_outdated="${_builddir}/tmp/outdated"
-	_steps="${_builddir}/steps"
+	_n="$(step_failures "$_steps")"
+	[ "$_n" -gt 0 ] || return 0
 
-	# After the outdated step, adjust step ids for any following skipped
-	# steps since the number of steps has most likely increased after
-	# finding all outdated ports.
-	if [ "$_name" = "outdated" ]; then
-		_n="$(wc -l "$_outdated" | awk '{print $1}')"
-		step_eval -n "outdated" "${_builddir}/steps"
-		_s="$(step_value step)"; _s=$((_s + 1))
-		cp "${_builddir}/steps" "${_builddir}/tmp/steps"
-		while step_eval "$_s" "${_builddir}/tmp/steps" 2>/dev/null; do
-			step_skip || break
-
-			step_end -S -n "$(step_value name)" -s "$((_s + _n))" \
-				"$_steps"
-			_s=$((_s + 1))
-		done
-		rm "${_builddir}/tmp/steps"
+	if [ "$_n" -gt 1 ] &&
+	   step_eval -n dpb "$_steps" 2>/dev/null &&
+	   [ "$(step_value exit)" -ne 0 ]; then
+		_n=$((_n - 1))
 	fi
 
-	# Ignore ports build errors.
-	case "$(cat "$_outdated" 2>/dev/null)" in
-	*${_name}*)	return 0;;
-	*)		return "$_exit";;
-	esac
+	if [ "$_n" -gt 1 ]; then
+		echo "${_n} failures"
+	else
+		echo "${_n} failure"
+	fi
+}
+
+# ports_step_after -b build-dir -e step-exit -n step-name
+#
+# After step hook, exits 0 if we can continue.
+ports_step_after() {
+	local _exit
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		-b)	shift;;
+		-e)	shift; _exit="$1";;
+		-n)	shift;;
+		*)	break;;
+		esac
+		shift
+	done
+	: "${_exit:?}"
+
+	return "$_exit"
 }
 
 # ports_steps
 #
 # Get the step names in execution order.
 ports_steps() {
-	local _outdated="${BUILDDIR}/tmp/outdated"
-
-	# The outdated file will eventually be populated by the outdated step.
 	xargs printf '%s\n' <<-EOF
 	env
 	cvs
 	proot
-	outdated
-	$( ([ -s "$_outdated" ] && cat "$_outdated" ) || :)
+	${PORTS}
+	dpb
 	distrib
 	end
 	EOF

util.sh
@@ -125,8 +125,7 @@ config_load() {
 	# Variables only honored by robsd-ports.
 	if [ "$_MODE" = "robsd-ports" ]; then
 		CHROOT=""; export CHROOT
-		MAKE_JOBS="1"; export MAKE_JOBS
-		NOPARALLEL=""; export NOPARALLEL
+		MAKE_JOBS="0"; export MAKE_JOBS
 		PORTS=""; export PORTS
 		PORTSDIR="/usr/ports"; export PORTSDIR
 		PORTSUSER=""; export PORTSUSER
@@ -544,8 +543,16 @@ duration_total() {
 	local _steps
 	local _tot=0
 
-	_steps="$1"
-	: "${_steps:?}"
+	_steps="$1"; : "${_steps:?}"
+
+	case "$_MODE" in
+	robsd-ports)
+		ports_duration_total -s "$_steps"
+		return 0
+		;;
+	*)
+		;;
+	esac
 
 	while step_eval "$_i" "$_steps" 2>/dev/null; do
 		_i=$((_i + 1))
@@ -848,7 +855,7 @@ report() {
 	local _n
 	local _name
 	local _report
-	local _status="ok"
+	local _status
 	local _steps
 	local _tmp
 
@@ -869,9 +876,12 @@ report() {
 	# another already running build.
 	[ -s "$_steps" ] || return 1
 
-	# If any step failed, the build failed.
 	case "$_MODE" in
-	robsd-ports|robsd-regress)
+	robsd-ports)
+		_status="$(ports_report_status -s "$_steps")"
+		;;
+	robsd-regress)
+		# If any step failed, the build failed.
 		_n="$(step_failures "$_steps")"
 		if [ "$_n" -gt 1 ]; then
 			_status="${_n} failures"
@@ -892,6 +902,7 @@ report() {
 		done
 		;;
 	esac
+	: "${_status:="ok"}"
 
 	if step_eval -n end "$_steps" 2>/dev/null; then
 		_duration="$(step_value duration)"
@@ -1485,10 +1496,7 @@ step_exec() (
 	[ -t 0 ] || exec >/dev/null 2>&1
 
 	{
-		if [ "$_MODE" = "robsd-ports" ] && ! [ -e "$_exec" ]; then
-			"$_robsdexec" sh -eux "${EXECDIR}/${_MODE}-exec.sh" \
-				"$_step" || : >"$_fail"
-		elif [ "$_MODE" = "robsd-regress" ] && ! [ -e "$_exec" ]; then
+		if [ "$_MODE" = "robsd-regress" ] && ! [ -e "$_exec" ]; then
 			"$_robsdexec" sh -eux "${EXECDIR}/${_MODE}-exec.sh" \
 				"$_step" || : >"$_fail"
 

Makefile
@@ -26,9 +26,8 @@ SCRIPTS+=	robsd-kernel.sh
 SCRIPTS+=	robsd-patch.sh
 SCRIPTS+=	robsd-ports-cvs.sh
 SCRIPTS+=	robsd-ports-distrib.sh
+SCRIPTS+=	robsd-ports-dpb.sh
 SCRIPTS+=	robsd-ports-env.sh
-SCRIPTS+=	robsd-ports-exec.sh
-SCRIPTS+=	robsd-ports-outdated.sh
 SCRIPTS+=	robsd-ports-proot.sh
 SCRIPTS+=	robsd-reboot.sh
 SCRIPTS+=	robsd-regress-env.sh
@@ -64,9 +63,8 @@ DISTFILES+=	robsd-patch.sh
 DISTFILES+=	robsd-ports
 DISTFILES+=	robsd-ports-cvs.sh
 DISTFILES+=	robsd-ports-distrib.sh
+DISTFILES+=	robsd-ports-dpb.sh
 DISTFILES+=	robsd-ports-env.sh
-DISTFILES+=	robsd-ports-exec.sh
-DISTFILES+=	robsd-ports-outdated.sh
 DISTFILES+=	robsd-ports-proot.sh
 DISTFILES+=	robsd-ports.8
 DISTFILES+=	robsd-reboot.sh
@@ -97,7 +95,6 @@ DISTFILES+=	tests/duration-total.sh
 DISTFILES+=	tests/format-duration.sh
 DISTFILES+=	tests/lock-acquire.sh
 DISTFILES+=	tests/log-id.sh
-DISTFILES+=	tests/ports-parallel.sh
 DISTFILES+=	tests/ports-report-log.sh
 DISTFILES+=	tests/ports-report-skip.sh
 DISTFILES+=	tests/prev-release.sh

robsd-ports-dpb.sh
@@ -1,0 +1,46 @@
+. "${EXECDIR}/util.sh"
+
+# duration pattern
+duration() {
+	local _pattern
+
+	_pattern="$1"; : "${_pattern:?}"
+	grep -m 1 "$_pattern" | awk '{print $NF}' | sed 's/\..*//'
+}
+
+PATH="${CHROOT}${PORTSDIR}/infrastructure/bin:${PATH}"
+
+_arch="$(machine)"
+
+_parallel=""
+if [ "$MAKE_JOBS" -gt 0 ]; then
+	_parallel="${MAKE_JOBS:+"-j ${MAKE_JOBS} -p ${MAKE_JOBS}"}"
+fi
+unset MAKE_JOBS
+
+xargs -t rm -rf <<EOF
+${CHROOT}${PORTSDIR}/logs/${_arch}
+${CHROOT}${PORTSDIR}/distfiles/build-stats/${_arch}
+EOF
+
+# shellcheck disable=SC2086
+dpb -c -B "$CHROOT" $_parallel $PORTS
+
+_fail=0
+for _p in $PORTS; do
+	_src="${CHROOT}${PORTSDIR}/logs/${_arch}/paths/${_p}.log"
+	[ -e "$_src" ] || continue
+
+	_id="$(step_id "$_p")"
+	_dst="${BUILDDIR}/$(log_id -b "$BUILDDIR" -n "$_p" -s "$_id")"
+	cp "$_src" "$_dst"
+
+	_t0="$(duration '^>>> Running ' <"$_src")"
+	_t1="$(duration '^>>> Ended ' <"$_src")"
+	_exit="$(! grep -q '^Error: job failed ' "$_src"; echo $?)"
+	[ "$_exit" -eq 0 ] || _fail=1
+	HOOK="" step_end -d "$((_t1 - _t0))" -e "$_exit" -l "$_dst" \
+		-n "$_p" -s "$_id" "${BUILDDIR}/steps"
+done
+
+exit "$_fail"

robsd-ports-exec.sh
@@ -1,21 +1,0 @@
-. "${EXECDIR}/util.sh"
-
-ports_parallel "$1" || unset MAKE_JOBS
-
-chroot "$CHROOT" sh -esux -- "$1" <<'EOF'
-PROGRESS_METER=No; export PROGRESS_METER
-
-_make="env "SUBDIR=${1}" make -C ${PORTSDIR}"
-$_make clean=all
-$_make package
-$_make install
-EOF
-
-[ -n "$SIGNIFY" ] || exit 0
-
-# By default pkg_sign(1) writes out the signed package to the current directory,
-# hence the cd.
-chroot "$CHROOT" env "SUBDIR=${1}" make -C "$PORTSDIR" show=PKGFILES |
-grep -v '^===> ' |
-xargs printf "${CHROOT}%s\n" |
-xargs -t -I{} sh -c "cd {}/.. && pkg_sign -s signify2 -s ${SIGNIFY} {}"

robsd-ports-outdated.sh
@@ -1,121 +1,0 @@
-. "${EXECDIR}/util.sh"
-
-set -o pipefail
-
-chroot "$CHROOT" sh -ex <<'EOF' | tsort -r >"${BUILDDIR}/tmp/outdated"
-# outdated port
-outdated() {
-	local _port
-	local _pkgfile
-
-	set -x
-
-	_port="$1"; : "${_port:?}"
-
-	_pkgfile="$(env "SUBDIR=${_port}" make show=PKGFILE | grep -v '^===> ')"
-	[ -e "$_pkgfile" ] || return 0
-
-	_s1=$(pkg_info -S -Dunsigned $_pkgfile 2>&1 | sed -n -e 's/Signature: //p')
-	_s2="$(env "SUBDIR=${_port}" make print-update-signature | grep -v '^===> ')"
-	[ "$_s1" != "$_s2" ]
-}
-
-# printgraph port ...
-printgraph() {
-	local _a
-	local _prev
-
-	_prev="$1"; shift
-	for _a; do
-		echo "${_prev} ${_a}"
-		_prev="$_a"
-	done
-}
-
-# walk
-walk()
-{
-	local _input="/tmp/walk"
-	local _path="/tmp/path"
-	local _port
-
-	set -x
-
-	cat >"$_input"
-	while [ -s "$_input" ]; do
-		read -r _port _ <"$_input"
-		walk1 -i "$_input" -p "$_path" "$_port"
-	done
-	rm "$_input" "$_path"
-}
-
-# walk1 -i input -p path port
-walk1()
-{
-	local _base
-	local _dependency
-	local _input
-	local _path
-	local _port
-
-	set -x
-
-	while [ $# -gt 0 ]; do
-		case "$1" in
-		-i)	shift; _input="$1";;
-		-p)	shift; _path="$1";;
-		*)	break;;
-		esac
-		shift
-	done
-	: "${_input:?}"
-	: "${_path:?}"
-	_port="$1"; : "${_port:?}"
-
-	# Discard multi packages and always build everything. Not sure if this a
-	# good idea.
-	_base="${_port%,-*}"
-	echo "$_base" >>"$_path"
-
-	if grep -q "$_base" "$_checked"; then
-		# Dependency already flagged as up-to-date.
-		:
-	elif grep -q "$_base" "$_outdated"; then
-		# Dependency already flagged as outdated, implies that
-		# the port is also outdated.
-		printgraph $(<"$_path")
-	elif outdated "$_base"; then
-		# Dependency outdated, implies that the port is also
-		# outdated.
-		echo "$_base" >>"$_outdated"
-		printgraph $(<"$_path")
-	else
-		# Dependency up-to-date, take note.
-		echo "$_base" >>"$_checked"
-	fi
-
-	while [ "$(head -1 "$_input" | cut -d ' ' -f 1)" = "$_port" ]; do
-		read -r _ _dependency <"$_input"
-		sed -i -e '1d' "$_input"
-		walk1 -i "$_input" -p "$_path" "$_dependency"
-	done
-
-	sed -i -e '$d' "$_path" || echo nein!
-}
-
-cd $PORTSDIR
-
-_DEPENDS_CACHE=$(make create_DEPENDS_CACHE); export _DEPENDS_CACHE
-
-_checked="/tmp/checked"; : >"$_checked"
-_depends="/tmp/depends"; : >"$_depends"
-_outdated="/tmp/outdated"; : >"$_outdated"
-_tsort="/tmp/tsort"; : >"$_tsort"
-
-env "SUBDIR=${PORTS}" make all-dir-depends |
-tee "$_depends" |
-walk |
-tee "$_tsort"
-
-make destroy_DEPENDS_CACHE
-EOF

robsd-ports.8
@@ -18,7 +18,8 @@ The
 .Nm
 utility builds
 .Ox
-ports.
+ports using
+.Xr dpb 1 .
 The process is divided into the steps as follows:
 .Bl -tag -width outdated
 .It env
@@ -35,15 +36,11 @@ Create the
 using
 .Xr proot 1 ,
 this is where ports are built.
-.It outdated
-Determine which ports among the configured
-.Ev PORTS
-that must be built.
-A port is considered outdated if the
-.Ev PKGFILE
-is absent or the package signature changed.
 .It Ev PORTS
-Build all outdated ports synchronously.
+Build all configured
+.Ev PORTS
+using
+.Xr dpb 1 .
 .It distrib
 Distribute the packages for all configured
 .Ev PORTS
@@ -110,8 +107,6 @@ Path to comment included in
 .It Fl r Ar path
 Resume a ports build located at
 .Ar path .
-If the last port failed or aborted, it will be built again.
-Otherwise, the next port will be built.
 .It Fl s Ar step
 Skip
 .Ar step ,

commit 1489e4f7d6ba5adea1fc5e196f01b91ab32aa362

fix robsd-ports regressions
Introduced in commit 87f8532 ("make use of dpb under the hood in robsd-ports").

robsd-ports
@@ -63,12 +63,6 @@ if [ "$STEP" -eq 1 ]; then
 			_id="$(step_id "$_step")"
 			step_end -S -n "$_step" -s "$_id" "${BUILDDIR}/steps"
 		done
-		# Flag all ports as skipped initially, later populated by the
-		# dpb step.
-		for _p in $PORTS; do
-			_id="$(step_id "$_p")"
-			step_end -S -n "$_p" -s "$_id" "${BUILDDIR}/steps"
-		done
 	fi
 
 	if [ -n "$TAGS" ]; then

robsd-ports-distrib.sh
@@ -6,26 +6,35 @@ if [ -z "$DISTRIBHOST" ] || [ -z "$DISTRIBPATH" ] || [ -z "$DISTRIBUSER" ]; then
 	exit 0
 fi
 
+_tmpdir="${BUILDDIR}/tmp"
+
 echo "$PORTS" |
 chroot "$CHROOT" env SUBDIRLIST=/dev/stdin make -C "$PORTSDIR" run-dir-depends |
 tsort |
 chroot "$CHROOT" env SUBDIRLIST=/dev/stdin make -C "$PORTSDIR" show=PKGFILES |
 grep -v '^===> ' |
 xargs printf "${CHROOT}%s\n" |
-sort |
-tee "${BUILDDIR}/tmp/distrib" |
-xargs sha256 |
+sort >"${_tmpdir}/distrib"
+
+if [ -n "$SIGNIFY" ]; then
+	# By default pkg_sign(1) writes out the signed package to the current
+	# directory, hence the cd.
+	xargs -I{} sh -c "cd {}/.. && pkg_sign -s signify2 -s ${SIGNIFY} {}" \
+		<"${_tmpdir}/distrib"
+fi
+
+xargs sha256 <"${_tmpdir}/distrib" |
 sed -e 's,(.*/,(,' >"${BUILDDIR}/tmp/SHA256"
 
 if [ -n "$SIGNIFY" ]; then
 	signify -Se -s "$SIGNIFY" -m "${BUILDDIR}/tmp/SHA256"
 fi
 
-xargs ls -nT <"${BUILDDIR}/tmp/distrib" |
+xargs ls -nT <"${_tmpdir}/distrib" |
 sed -e 's,/.*/,,' >"${BUILDDIR}/tmp/index.txt"
 
 unpriv "$DISTRIBUSER" <<EOF
 ssh -n ${DISTRIBHOST} rm -f ${DISTRIBPATH}/*
-cd ${BUILDDIR}/tmp
+cd ${_tmpdir}
 scp -p \$(<distrib) SHA256* index.txt ${DISTRIBHOST}:${DISTRIBPATH}
 EOF

util-ports.sh
@@ -1,28 +1,3 @@
-# ports_begin -n step -s steps-file
-#
-# Exits 0 if the given step can be executed.
-ports_begin() {
-	local _name
-	local _steps
-
-	while [ $# -gt 0 ]; do
-		case "$1" in
-		-n)	shift; _name="$1";;
-		-s)	shift; _steps="$1";;
-		*)	break;;
-		esac
-		shift
-	done
-	: "${_name:?}"
-	: "${_steps:?}"
-
-	if [ "$_name" = "distrib" ] &&
-	   [ "$(step_failures "$_steps")" -gt 0 ]; then
-		return 1
-	fi
-	return 0
-}
-
 # ports_config_load
 #
 # Handle ports specific configuration.
@@ -201,6 +176,25 @@ ports_step_after() {
 	: "${_exit:?}"
 
 	return "$_exit"
+}
+
+# ports_step_skip -n step-name
+#
+# Exits zero if the step has been skipped.
+ports_step_skip() {
+	local _name
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		-n)	shift; _name="$1";;
+		*)	break;;
+		esac
+		shift
+	done
+	: "${_name:?}"
+
+	# Ports are already covered the dpb step.
+	echo "$PORTS" | grep -q "\<${_name}\>"
 }
 
 # ports_steps

util.sh
@@ -1234,12 +1234,20 @@ robsd() {
 		_s="$_step"
 		_step=$((_step + 1))
 
-		if step_eval -n "$_name" "${BUILDDIR}/steps" 2>/dev/null && step_skip; then
+		if step_eval -n "$_name" "${BUILDDIR}/steps" 2>/dev/null &&
+		   step_skip; then
 			info "step ${_name} skipped"
 			continue
-		else
-			info "step ${_name}"
 		fi
+
+		case "$_MODE" in
+		robsd-ports)
+			ports_step_skip -n "$_name" && continue
+			;;
+		*)
+			;;
+		esac
+		info "step ${_name}"
 
 		if [ "$_name" = "end" ]; then
 			# The duration of the end step is the accumulated
@@ -1248,14 +1256,6 @@ robsd() {
 				-n "$_name" -s "$_s" "${BUILDDIR}/steps"
 			return 0
 		fi
-
-		case "$_MODE" in
-		robsd-ports)
-			ports_begin -n "$_name" -s "${BUILDDIR}/steps" || return 1
-			;;
-		*)
-			;;
-		esac
 
 		_log="${BUILDDIR}/$(log_id -b "$BUILDDIR" -n "$_name" -s "$_s")"
 		_exit=0