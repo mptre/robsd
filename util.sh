mptre/robsd

commit 38467b97a638bb4ca6608c53f82cb3bae4933dba

invert detach option

util.sh
@@ -100,7 +100,7 @@ config_load() {
 	BUILDUSER="build"; export BUILDUSER
 	CVSROOT=""; export CVSROOT
 	CVSUSER=""; export CVSUSER
-	DETACH=0
+	DETACH=1
 	DISTRIBHOST=""; export DISTRIBHOST
 	DISTRIBPATH=""; export DISTRIBPATH
 	DISTRIBUSER=""; export DISTRIBUSER

robsd
@@ -5,7 +5,7 @@ set -eu
 usage() {
 	xargs 1>&2 <<-EOF
 	usage: robsd
-	[-D]
+	[-d]
 	[-S src-diff]
 	[-X xenocara-diff]
 	[-c comment]
@@ -27,9 +27,9 @@ COMMENT=""
 STATPID=""
 STEP=1
 
-while getopts "DS:X:c:r:s:t:" opt; do
+while getopts "dS:X:c:r:s:t:" opt; do
 	case "$opt" in
-	D)	DETACH=1;;
+	d)	DETACH=0;;
 	S)	BSDDIFF="${BSDDIFF}${BSDDIFF:+ }$(readlink -f "${OPTARG}")";;
 	X)	XDIFF="${XDIFF}${XDIFF:+ }$(readlink -f "${OPTARG}")";;
 	c)	COMMENT="$OPTARG";;

robsd-ports
@@ -5,7 +5,7 @@ set -eu
 usage() {
 	xargs 1>&2 <<-EOF
 	usage: robsd-ports
-	[-D]
+	[-d]
 	[-c comment]
 	[-r path]
 	[-s step]
@@ -25,9 +25,9 @@ COMMENT=""
 STATPID=""
 STEP=1
 
-while getopts "Dc:r:s:t:" opt; do
+while getopts "dc:r:s:t:" opt; do
 	case "$opt" in
-	D)	DETACH=1;;
+	d)	DETACH=0;;
 	c)	COMMENT="$OPTARG";;
 	r)	BUILDDIR="$(readlink -f "$OPTARG")";;
 	s)	SKIP="${SKIP}${SKIP:+ }${OPTARG}";;

robsd-ports.8
@@ -6,12 +6,12 @@
 .Nd build OpenBSD ports
 .Sh SYNOPSIS
 .Nm robsd-ports
-.Op Fl D
+.Op Fl d
 .Op Fl c Ar comment
 .Op Fl s Ar step
 .Op Fl t Ar tag
 .Nm robsd-ports
-.Op Fl D
+.Op Fl d
 .Fl r Ar path
 .Sh DESCRIPTION
 The
@@ -96,8 +96,8 @@ to ensure a clean exit.
 .Pp
 The options are as follows:
 .Bl -tag -width Ds
-.It Fl D
-Detach
+.It Fl d
+Do not detach
 .Nm
 into the background.
 .It Fl c Ar comment

robsd-regress
@@ -5,7 +5,7 @@ set -eu
 usage() {
 	xargs 1>&2 <<-EOF
 	usage: robsd-regress
-	[-D]
+	[-d]
 	[-c comment]
 	[-r path]
 	[-t tag]
@@ -24,9 +24,9 @@ COMMENT=""
 STATPID=""
 STEP=1
 
-while getopts "Dc:r:t:" opt; do
+while getopts "dc:r:t:" opt; do
 	case "$opt" in
-	D)	DETACH=1;;
+	d)	DETACH=0;;
 	c)	COMMENT="$OPTARG";;
 	r)	BUILDDIR="$(readlink -f "$OPTARG")";;
 	t)	TAGS="${TAGS}${TAGS:+ }${OPTARG}";;

robsd-regress.8
@@ -6,11 +6,11 @@
 .Nd run OpenBSD regression tests
 .Sh SYNOPSIS
 .Nm robsd-regress
-.Op Fl D
+.Op Fl d
 .Op Fl c Ar comment
 .Op Fl t Ar tag
 .Nm
-.Op Fl D
+.Op Fl d
 .Fl r Ar path
 .Sh DESCRIPTION
 The
@@ -75,8 +75,8 @@ to ensure a clean exit.
 .Pp
 The options are as follows:
 .Bl -tag -width Ds
-.It Fl D
-Detach
+.It Fl d
+Do not detach
 .Nm
 into the background.
 .It Fl c Ar comment

robsd.8
@@ -6,14 +6,14 @@
 .Nd build OpenBSD release
 .Sh SYNOPSIS
 .Nm robsd
-.Op Fl D
+.Op Fl d
 .Op Fl S Ar src-diff
 .Op Fl X Ar xenocara-diff
 .Op Fl c Ar comment
 .Op Fl s Ar step
 .Op Fl t Ar tag
 .Nm
-.Op Fl D
+.Op Fl d
 .Fl r Ar path
 .Sh DESCRIPTION
 The
@@ -138,8 +138,8 @@ to ensure a clean exit.
 .Pp
 The options are as follows:
 .Bl -tag -width Ds
-.It Fl D
-Detach
+.It Fl d
+Do not detach
 .Nm
 into the background.
 .It Fl S Ar src-diff

tests/robsd-ports.sh
@@ -27,7 +27,7 @@ if testcase "basic"; then
 	mkdir "$ROBSDDIR"
 
 	if ! PATH="${BINDIR}:${PATH}" WRKDIR="$WRKDIR" sh "$ROBSDPORTS" \
-	   -s cvs -s proot >"$TMP1" 2>&1; then
+	   -d -s cvs -s proot >"$TMP1" 2>&1; then
 		fail - "expected exit zero" <"$TMP1"
 	fi
 	_builddir="$(find "${ROBSDDIR}" -type d -mindepth 1 -maxdepth 1)"
@@ -62,7 +62,7 @@ if testcase "skip"; then
 	mkdir "$ROBSDDIR"
 
 	if ! PATH="${BINDIR}:${PATH}" WRKDIR="$WRKDIR" sh "$ROBSDPORTS" \
-	   -s cvs -s proot -s distrib >"$TMP1" 2>&1; then
+	   -d -s cvs -s proot -s distrib >"$TMP1" 2>&1; then
 		fail - "expected exit zero" <"$TMP1"
 	fi
 	_builddir="$(find "${ROBSDDIR}" -type d -mindepth 1 -maxdepth 1)"

commit 422bbb8aedf517ae8211d2534ab7103db30c2ff8

remove usage of deleted detach option
Regression introduced in commit 38467b9 ("invert detach option").

util.sh
@@ -853,7 +853,7 @@ purge() {
 # Commence reboot and continue building the current release after boot.
 reboot_commence() {
 	cat <<-EOF >>/etc/rc.firsttime
-	/usr/local/sbin/robsd -D -r ${BUILDDIR} >/dev/null
+	/usr/local/sbin/robsd -r ${BUILDDIR} >/dev/null
 	EOF
 
 	# Add some grace in order to let the script finish.