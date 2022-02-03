a656832fafe9997f2806e159a3a68f1186e58500
Anton Lindqvist <anton@basename.se>
2022-02-03 16:32:46 +0100

    sync distfiles

--- mkls.sh
+++ mkls.sh
@@ -5,7 +5,8 @@ SCRIPTS \
 DISTFILES \
 	*.c *.md !(mkls).sh \
 	robsd?(-clean|-kill|-ports|-regress|-rescue) \
-	robsd?(.conf|-clean|-kill|-ports|-regress|-rescue|-stat).[0-9] \
+	robsd*.conf.5 \
+	robsd?(-clean|-kill|-ports|-regress|-rescue|-stat).[0-9] \
 	LICENSE Makefile Makefile.inc \
 	tests/*.sh \
 	tests/Makefile
