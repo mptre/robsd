mptre/robsd

commit d9340993743ff0ec56b133228701514f2c706921

rework configuration
Introduce the robsd-config utility in charge of the configuration. This gives a
better view of the configuration required by each step as the configuration no
longer is expressed using global variables.

robsd-regress-exec.sh
@@ -1,5 +1,10 @@
 . "${EXECDIR}/util.sh"
 
+config_load <<'EOF'
+BSDSRCDIR="${bsd-srcdir}"
+REGRESSUSER="${regress-user}"
+EOF
+
 if regress_root "$1"; then
 	unset SUDO
 	exec make -C "${BSDSRCDIR}/regress/${1}"

Makefile
@@ -1,5 +1,10 @@
 VERSION=	7.3.0
 
+PROG_robsd-config=	robsd-config
+SRCS_robsd-config=	robsd-config.c
+OBJS_robsd-config=	${SRCS_robsd-config:.c=.o}
+DEPS_robsd-config=	${SRCS_robsd-config:.c=.d}
+
 PROG_robsd-exec=	robsd-exec
 SRCS_robsd-exec=	robsd-exec.c
 OBJS_robsd-exec=	${SRCS_robsd-exec:.c=.o}
@@ -12,6 +17,7 @@ DEPS_robsd-stat=	${SRCS_robsd-stat:.c=.d}
 
 CFLAGS+=	-Wall -Wextra -MD -MP
 
+KNFMT+=	robsd-config.c
 KNFMT+=	robsd-exec.c
 KNFMT+=	robsd-stat.c
 
@@ -49,6 +55,7 @@ DISTFILES+=	robsd-base.sh
 DISTFILES+=	robsd-checkflist.sh
 DISTFILES+=	robsd-clean
 DISTFILES+=	robsd-clean.8
+DISTFILES+=	robsd-config.c
 DISTFILES+=	robsd-cvs.sh
 DISTFILES+=	robsd-distrib.sh
 DISTFILES+=	robsd-env.sh
@@ -82,7 +89,6 @@ DISTFILES+=	robsd.8
 DISTFILES+=	robsd.conf.5
 DISTFILES+=	tests/Makefile
 DISTFILES+=	tests/cleandir.sh
-DISTFILES+=	tests/config-load.sh
 DISTFILES+=	tests/cvs-log.sh
 DISTFILES+=	tests/diff-apply.sh
 DISTFILES+=	tests/diff-clean.sh
@@ -97,13 +103,13 @@ DISTFILES+=	tests/ports-report-log.sh
 DISTFILES+=	tests/ports-report-skip.sh
 DISTFILES+=	tests/prev-release.sh
 DISTFILES+=	tests/purge.sh
-DISTFILES+=	tests/regress-config-load.sh
 DISTFILES+=	tests/regress-failed.sh
 DISTFILES+=	tests/regress-report-log.sh
 DISTFILES+=	tests/report-duration.sh
 DISTFILES+=	tests/report-size.sh
 DISTFILES+=	tests/report-skip.sh
 DISTFILES+=	tests/report.sh
+DISTFILES+=	tests/robsd-config.sh
 DISTFILES+=	tests/robsd-ports.sh
 DISTFILES+=	tests/robsd-regress.sh
 DISTFILES+=	tests/robsd-rescue.sh
@@ -128,7 +134,9 @@ INSTALL_MAN?=	${INSTALL}
 
 MANLINT+=	robsd-clean.8
 MANLINT+=	robsd-ports.8
+MANLINT+=	robsd-ports.conf.5
 MANLINT+=	robsd-regress.8
+MANLINT+=	robsd-regress.conf.5
 MANLINT+=	robsd-rescue.8
 MANLINT+=	robsd-stat.8
 MANLINT+=	robsd.8
@@ -144,7 +152,10 @@ SHLINT+=	robsd-rescue
 
 SUBDIR+=	tests
 
-all: ${PROG_robsd-exec} ${PROG_robsd-stat}
+all: ${PROG_robsd-config} ${PROG_robsd-exec} ${PROG_robsd-stat}
+
+${PROG_robsd-config}: ${OBJS_robsd-config}
+	${CC} ${DEBUG} -o ${PROG_robsd-config} ${OBJS_robsd-config} ${LDFLAGS}
 
 ${PROG_robsd-exec}: ${OBJS_robsd-exec}
 	${CC} ${DEBUG} -o ${PROG_robsd-exec} ${OBJS_robsd-exec} ${LDFLAGS}
@@ -154,6 +165,7 @@ ${PROG_robsd-stat}: ${OBJS_robsd-stat}
 
 clean:
 	rm -f \
+		${DEPS_robsd-config} ${OBJS_robsd-config} ${PROG_robsd-config} \
 		${DEPS_robsd-exec} ${OBJS_robsd-exec} ${PROG_robsd-exec} \
 		${DEPS_robsd-stat} ${OBJS_robsd-stat} ${PROG_robsd-stat}
 .PHONY: clean
@@ -189,6 +201,8 @@ install: all
 	${INSTALL_MAN} ${.CURDIR}/robsd-clean.8 ${DESTDIR}${MANDIR}/man8
 	${INSTALL_MAN} ${.CURDIR}/robsd-kill.8 ${DESTDIR}${MANDIR}/man8
 	${INSTALL_MAN} ${.CURDIR}/robsd-rescue.8 ${DESTDIR}${MANDIR}/man8
+# robsd-config
+	${INSTALL} -m 0555 ${PROG_robsd-config} ${DESTDIR}${LIBEXECDIR}/robsd
 # robsd-exec
 	${INSTALL} -m 0555 ${PROG_robsd-exec} ${DESTDIR}${LIBEXECDIR}/robsd
 # robsd-ports
@@ -196,13 +210,16 @@ install: all
 	ln -f ${DESTDIR}${BINDIR}/robsd-clean ${DESTDIR}${BINDIR}/robsd-ports-clean
 	ln -f ${DESTDIR}${BINDIR}/robsd-kill ${DESTDIR}${BINDIR}/robsd-ports-kill
 	ln -f ${DESTDIR}${LIBEXECDIR}/robsd/robsd-exec ${DESTDIR}${LIBEXECDIR}/robsd/robsd-ports-exec
-	${INSTALL} -m 0555 ${PROG_robsd-exec} ${DESTDIR}${LIBEXECDIR}/robsd
+	ln -f ${DESTDIR}${LIBEXECDIR}/robsd/robsd-config ${DESTDIR}${LIBEXECDIR}/robsd/robsd-ports-config
+	${INSTALL_MAN} ${.CURDIR}/robsd-ports.conf.5 ${DESTDIR}${MANDIR}/man5
 	${INSTALL_MAN} ${.CURDIR}/robsd-ports.8 ${DESTDIR}${MANDIR}/man8
 # robsd-regress
 	${INSTALL} -m 0555 ${.CURDIR}/robsd-regress ${DESTDIR}${BINDIR}
 	ln -f ${DESTDIR}${BINDIR}/robsd-clean ${DESTDIR}${BINDIR}/robsd-regress-clean
 	ln -f ${DESTDIR}${BINDIR}/robsd-kill ${DESTDIR}${BINDIR}/robsd-regress-kill
 	ln -f ${DESTDIR}${LIBEXECDIR}/robsd/robsd-exec ${DESTDIR}${LIBEXECDIR}/robsd/robsd-regress-exec
+	ln -f ${DESTDIR}${LIBEXECDIR}/robsd/robsd-config ${DESTDIR}${LIBEXECDIR}/robsd/robsd-regress-config
+	${INSTALL_MAN} ${.CURDIR}/robsd-regress.conf.5 ${DESTDIR}${MANDIR}/man5
 	${INSTALL_MAN} ${.CURDIR}/robsd-regress.8 ${DESTDIR}${MANDIR}/man8
 # robsd-stat
 	${INSTALL} -m 0555 ${PROG_robsd-stat} ${DESTDIR}${LIBEXECDIR}/robsd
@@ -212,11 +229,13 @@ install: all
 test: all
 	${MAKE} -C ${.CURDIR}/tests \
 		"EXECDIR=${.CURDIR}" \
+		"ROBSDCONFIG=${.OBJDIR}/${PROG_robsd-config}" \
 		"ROBSDEXEC=${.OBJDIR}/${PROG_robsd-exec}" \
 		"ROBSDSTAT=${.OBJDIR}/${PROG_robsd-stat}" \
 		"TESTFLAGS=${TESTFLAGS}"
 .PHONY: test
 
 .include "${.CURDIR}/Makefile.inc"
+-include ${DEPS_robsd-config}
 -include ${DEPS_robsd-exec}
 -include ${DEPS_robsd-stat}

robsd
@@ -21,7 +21,16 @@ usage() {
 setmode "robsd"
 setprogname "robsd"
 [ "$(id -u)" -ne 0 ] && fatal "must be run as root"
-config_load "$(config_path)"
+
+config_load <<'EOF'
+ROBSDDIR="${robsddir}"
+KEEP="${keep}"
+SKIP="${skip}"
+BSDDIFF="${bsd-diff}"
+BSDSRCDIR="${bsd-srcdir}"
+XDIFF="${x11-diff}"
+XSRCDIR="${x11-srcdir}"
+EOF
 
 _comment=""
 _statpid=""

robsd-base.sh
@@ -1,4 +1,9 @@
-unset DESTDIR
+. "${EXECDIR}/util.sh"
+
+config_load <<-'EOF'
+BSDOBJDIR="${bsd-objdir}"; export BSDOBJDIR
+BSDSRCDIR="${bsd-srcdir}"; export BSDSRCDIR
+EOF
 
 chown build:wobj "$BSDOBJDIR"
 chmod 770 "$BSDOBJDIR"

robsd-checkflist.sh
@@ -1,4 +1,11 @@
-DESTDIR="${DESTDIR}/src"
+. "${EXECDIR}/util.sh"
+
+config_load <<'EOF'
+DESTDIR="${destdir}"
+BSDSRCDIR="${bsd-srcdir}"
+EOF
+
+DESTDIR="${DESTDIR}/src"; export DESTDIR
 
 cd "${BSDSRCDIR}/distrib/sets"
 sh checkflist || :

robsd-clean
@@ -14,7 +14,9 @@ usage() {
 setmode "$(getmode "$0")"
 setprogname "${_MODE}-clean"
 [ "$(id -u)" -ne 0 ] && fatal "must be run as root"
-config_load "$(config_path)"
+config_load <<'EOF'
+ROBSDDIR="${robsddir}"
+EOF
 
 purge "$ROBSDDIR" "$1" | while read -r _d; do
 	info "moving ${_d} to the attic"

robsd-cvs.sh
@@ -1,5 +1,29 @@
 . "${EXECDIR}/util.sh"
 
+config_load <<'EOF'
+ROBSDDIR="${robsddir}"
+CVSUSER="${cvs-user}"
+CVSROOT="${cvs-root}"; export CVSROOT
+EOF
+
+case "$_MODE" in
+robsd)
+	config_load <<-'EOF'
+	BSDSRCDIR="${bsd-srcdir}"
+	XSRCDIR="${x11-srcdir}"
+	EOF
+	;;
+robsd-ports)
+	config_load <<-'EOF'
+	CHROOT="${chroot}"
+	PORTSDIR="${ports-dir}"
+	EOF
+	;;
+*)
+	exit 1
+	;;
+esac
+
 _tmpdir="${BUILDDIR}/tmp"
 
 {
@@ -12,7 +36,7 @@ _tmpdir="${BUILDDIR}/tmp"
 
 	unpriv "$CVSUSER" "cd ${_d} && exec cvs -q -d ${CVSROOT} update -Pd" 2>&1 |
 	tee "$_up" |
-	cvs_log -r "$_d" -t "$_tmpdir" -u "$CVSUSER" |
+	cvs_log -r "$ROBSDDIR" -t "$_tmpdir" -u "$CVSUSER" -c "$_d" |
 	tee "$_ci"
 
 	find "$_d" -type f -name Root -delete

commit d292b4e7b47643de9a148fa82442e2705ae98c64

fix sudo regression
Add missing export of sudo, regression introduced in commit ("d934099 rework
configuration"). This is exactly the kind of implicit dependencies I was hoping
to discover and shed some light on.

While here, fix odd whitespace (i.e. tabs) in sudo default value.

robsd-config.c
@@ -214,7 +214,7 @@ static const struct grammar robsd_regress[] = {
 	{ "hook",		STRING,		parser_string,	0,		NULL },
 	{ "keep",		INTEGER,	parser_integer,	0,		NULL },
 	{ "rdonly",		INTEGER,	parser_integer,	0,		NULL },
-	{ "sudo",		STRING,		parser_string,	0,		"doas	-n" },
+	{ "sudo",		STRING,		parser_string,	0,		"doas -n" },
 	{ "bsd-diff",		LIST,		parser_glob,	0,		NULL },
 	{ "bsd-srcdir",		STRING,		parser_dir,	0,		"/usr/src" },
 	{ "cvs-user",		STRING,		parser_user,	MANDATORY,	NULL },

robsd-regress-exec.sh
@@ -1,13 +1,14 @@
 . "${EXECDIR}/util.sh"
 
 config_load <<'EOF'
+SUDO="${sudo}"
 BSDSRCDIR="${bsd-srcdir}"
 REGRESSUSER="${regress-user}"
 EOF
 
 if regress_root "$1"; then
-	unset SUDO
 	exec make -C "${BSDSRCDIR}/regress/${1}"
 else
+	export SUDO
 	unpriv "$REGRESSUSER" "exec make -C ${BSDSRCDIR}/regress/${1}"
 fi