b1a9bfc431031e147a40433c34079c3a7246ebf9
Anton Lindqvist <anton@basename.se>
2022-05-09 14:07:02 +0200

    include failed and skipped test cases in report

--- robsd-regress-exec.sh
+++ robsd-regress-exec.sh
@@ -6,9 +6,20 @@ BSDSRCDIR="${bsd-srcdir}"
 REGRESSUSER="${regress-user}"
 EOF
 
+_err=0
+_log="${BUILDDIR}/tmp/regress"; : >"$_log"; chmod 666 "$_log"
+_make="make -C ${BSDSRCDIR}/regress/${1} REGRESS_LOG=${_log}"
 if regress_root "$1"; then
-	exec make -C "${BSDSRCDIR}/regress/${1}"
+	exec $_make || _err="$?"
 else
 	export SUDO
-	unpriv "$REGRESSUSER" "exec make -C ${BSDSRCDIR}/regress/${1}"
+	unpriv "$REGRESSUSER" "$_make" || _err="$?"
 fi
+
+_fail="$(sed -n -e "s,${1}/,," -e 's/^FAIL //p' "$_log" | xargs)"
+[ -z "$_fail" ] || echo "X-Fail: ${_fail}"
+_skip="$(sed -n -e "s,${1}/,," -e 's/^SKIP //p' "$_log" | xargs)"
+[ -z "$_skip" ] || echo "X-Skip: ${_skip}"
+
+rm -f "$_log"
+exit "$_err"
