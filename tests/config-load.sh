mptre/robsd

commit be5e5b9e3785a833514ac4b72d74f72472f034d9

propagate robsd-config errors

util.sh
@@ -106,7 +106,7 @@ config_load() {
 		cat
 		echo "EXECDIR=\${execdir}"
 	} | "$ROBSDCONFIG" -m "$_MODE" -b "${BUILDDIR}" \
-		${ROBSDCONF:+"-f${ROBSDCONF}"} - >"$_tmp"
+		${ROBSDCONF:+"-f${ROBSDCONF}"} - >"$_tmp" || return 1
 	eval "$(<"$_tmp")"
 	rm "$_tmp"
 

commit 9642686cd2ff833c5f0ca6b1dcf62b40b3ea274b

remove tmp file in case of config_load error
Regression introduced in commit be5e5b9 ("propagate robsd-config errors").

Makefile
@@ -150,6 +150,7 @@ DISTFILES+=	robsd.conf.5
 DISTFILES+=	tests/Makefile
 DISTFILES+=	tests/check-perf.sh
 DISTFILES+=	tests/cleandir.sh
+DISTFILES+=	tests/config-load.sh
 DISTFILES+=	tests/cvs-log.sh
 DISTFILES+=	tests/diff-apply.sh
 DISTFILES+=	tests/diff-clean.sh

util.sh
@@ -92,6 +92,7 @@ cleandir() {
 # Load and validate the configuration.
 # shellcheck disable=SC2120
 config_load() {
+	local _err=0
 	local _tmp
 
 	: "${BUILDDIR:=}"; export BUILDDIR
@@ -107,9 +108,10 @@ config_load() {
 		cat
 		echo "EXECDIR=\${execdir}"
 	} | "$ROBSDCONFIG" -m "$_MODE" ${ROBSDCONF:+"-f${ROBSDCONF}"} "$@" - \
-		>"$_tmp" || return 1
-	eval "$(<"$_tmp")"
+		>"$_tmp" || _err="$?"
+	[ "$_err" -eq 0 ] && eval "$(<"$_tmp")"
 	rm "$_tmp"
+	[ "$_err" -eq 0 ] || return "$_err"
 
 	case "$_MODE" in
 	robsd)

tests/Makefile
@@ -1,5 +1,6 @@
 TESTS+=	check-perf.sh
 TESTS+=	cleandir.sh
+TESTS+=	config-load.sh
 TESTS+=	cvs-log.sh
 TESTS+=	diff-apply.sh
 TESTS+=	diff-clean.sh

tests/config-load.sh
@@ -1,0 +1,12 @@
+if testcase "failure"; then
+	robsd_config - <<-EOF
+	robsddir "${TSHDIR}"
+	EOF
+
+	# shellcheck disable=SC2016
+	if echo '${undefined}' | TMPDIR=${TSHDIR} config_load >"$TMP1" 2>&1; then
+		fail - "expected exit non-zero" <"$TMP1"
+	fi
+	find "$TSHDIR" -type f -name 'robsd.*' \! -name 'robsd.conf' >"$TMP1"
+	assert_file - "$TMP1" </dev/null
+fi