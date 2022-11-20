7d7ce85cebba6e0c7957513343ad47a35a31daee
Anton Lindqvist <anton@basename.se>
2022-11-20 16:28:42 +0100

    rework step_end() tests
    Some of them are superseded by robsd-step by now.

--- tests/step-end.sh
+++ tests/step-end.sh
@@ -8,23 +8,11 @@ if testcase "basic"; then
 	step_end -t -d 2 -n cvs -s 2 "$TMP1"
 
 	step_eval -n cvs "$TMP1"
-	assert_eq "2" "$(step_value step)" "step"
+	assert_eq "2" "$(step_value duration)" "duration"
 	assert_eq "cvs" "$(step_value name)" "name"
+	assert_eq "2" "$(step_value step)" "step"
 	assert_eq "0" "$(step_value exit)" "exit"
-	assert_eq "2" "$(step_value duration)" "duration"
-fi
-
-if testcase "already present"; then
-	robsd_config - <<-EOF
-	robsddir "${TSHDIR}"
-	EOF
-	step_serialize -s 2 -n cvs >"$TMP1"
-
-	step_end -t -d 2 -n cvs -s 2 "$TMP1"
-
-	step_eval -n cvs "$TMP1"
-	assert_eq "2" "$(step_value duration)" "duration"
-	assert_eq "2" "$(wc -l "$TMP1" | awk '{print $1}')"
+	assert_eq "0" "$(step_value skip)" "skip"
 fi
 
 if testcase "skip"; then
@@ -32,7 +20,7 @@ if testcase "skip"; then
 	robsddir "${TSHDIR}"
 	EOF
 
-	step_end -S -t -n cvs -s 10 "$TMP1"
+	step_end -S -t -n cvs -s 2 "$TMP1"
 
 	step_eval -n cvs "$TMP1"
 	assert_eq "1" "$(step_value skip)" "skip"
