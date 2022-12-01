5ca973dba4b1b5523464800f7d77c0ef082b2a68
Anton Lindqvist <anton@basename.se>
2022-12-01 07:16:35 +0100

    get rid of old cvs_log() arguments
    Regression introduced in commit 1c2af73 ("add robsd-ls utility used to list
    invocations").

--- robsd-cvs.sh
+++ robsd-cvs.sh
@@ -55,7 +55,7 @@ _tmpdir="${BUILDDIR}/tmp"
 	else
 		unpriv "$CVSUSER" "cd ${_d} && exec cvs -qd ${CVSROOT} update -Pd" 2>&1 |
 		tee "$_up" |
-		cvs_log -b "$BUILDDIR" -r "$ROBSDDIR" -t "$_tmpdir" -c "$_d" -h "$CVSROOT" -u "$CVSUSER" |
+		cvs_log -t "$_tmpdir" -c "$_d" -h "$CVSROOT" -u "$CVSUSER" |
 		tee "$_ci"
 	fi
 
