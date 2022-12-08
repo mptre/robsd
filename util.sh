mptre/robsd

commit 0e7f9df952441051ab7efb8a9653dcea7aa529ea

make builddir available during robsd-config interpolation
Read from the lock file.

util.sh
@@ -1456,7 +1456,6 @@ step_end() {
 	if [ "$_d" -ne -1 ]; then
 		# Ignore non-zero exit.
 		"$ROBSDHOOK" -m "$_MODE" -V ${ROBSDCONF:+"-f${ROBSDCONF}"} \
-			-v "builddir=${BUILDDIR}" \
 			-v "exit=${_e}" \
 			-v "step=${_name}" \
 			|| :

config.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "buffer.h"
 #include "cdefs.h"
@@ -149,6 +150,7 @@ static int	config_parse_directory(struct config *, union variable_value *);
 static int	config_parse_nop(struct config *, union variable_value *);
 
 static int			 config_append_defaults(struct config *);
+static int			 config_append_build_dir(struct config *);
 static struct variable		*config_append(struct config *,
     enum variable_type, const char *, const union variable_value *, int,
     unsigned int);
@@ -1077,6 +1079,9 @@ config_append_defaults(struct config *cf)
 	config_append_string(cf, "arch", MACHINE_ARCH);
 	config_append_string(cf, "machine", MACHINE);
 
+	if (config_append_build_dir(cf))
+		return 1;
+
 	val.str = interpolate_str("${robsddir}/attic",
 	    &(struct interpolate_arg){
 		.lookup	= config_interpolate_lookup,
@@ -1094,6 +1099,52 @@ config_append_defaults(struct config *cf)
 	}
 
 	return 0;
+}
+
+static int
+config_append_build_dir(struct config *cf)
+{
+	struct buffer *bf = NULL;
+	char *nl, *path;
+	int error = 0;
+	int fd = -1;
+
+	path = interpolate_str("${robsddir}/.running",
+	    &(struct interpolate_arg){
+		.lookup	= config_interpolate_lookup,
+		.arg	= cf,
+	});
+	if (path == NULL)
+		return 1;
+
+	/*
+	 * The lock file is only expected to be present while robsd is running.
+	 * Therefore do not treat failures as fatal.
+	 */
+	fd = open(path, O_RDONLY | O_CLOEXEC);
+	if (fd == -1)
+		goto out;
+	bf = buffer_read_fd(fd);
+	if (bf == NULL) {
+		error = 1;
+		goto out;
+	}
+	buffer_putc(bf, '\0');
+	nl = strchr(bf->bf_ptr, '\n');
+	if (nl == NULL) {
+		warnx("%s: line not found", path);
+		error = 1;
+		goto out;
+	}
+	*nl = '\0';
+	config_append_string(cf, "builddir", bf->bf_ptr);
+
+out:
+	if (fd != -1)
+		close(fd);
+	buffer_free(bf);
+	free(path);
+	return error;
 }
 
 static struct variable *

robsd-config.8
@@ -25,6 +25,9 @@ all modes:
 .Bl -tag -compact -width keep-dir
 .It Ic arch
 CPU architecture.
+.It Ic builddir
+The current invocation directory rooted in
+.Ic robsddir .
 .It Ic machine
 Kernel architecture.
 .It Ic keep-dir

robsd-hook.5
@@ -17,14 +17,13 @@ configuration variable and may contain one or many arguments passed to
 .Xr execvp 3 .
 For each argument, variables on the form
 .Dq \(Do Ns Brq Ar variable
-are expanded where
+are interpolated where
 .Ar variable
-can refer to a configuration variable or one of the following:
+can refer to a configuration variable, see
+.Xr robsd-config 8 .
+In addition, the following variables are also available:
 .Pp
-.Bl -tag -compact -width builddir
-.It Ic builddir
-invocation directory rooted in
-.Ic robsddir
+.Bl -tag -compact -width exit
 .It Ic exit
 exit code of the step
 .It Ic step
@@ -37,6 +36,7 @@ hook { "echo" "step ${step} done" }
 .Sh SEE ALSO
 .Xr robsd-ports.conf 5 ,
 .Xr robsd-regress.conf 5 ,
-.Xr robsd.conf 5
+.Xr robsd.conf 5 ,
+.Xr robsd-config 8
 .Sh AUTHORS
 .An Anton Lindqvist Aq Mt anton@basename.se

tests/robsd-config.sh
@@ -38,7 +38,7 @@ robsd_config() {
 # default_config
 default_config() {
 	cat <<-EOF
-	robsddir "/tmp"
+	robsddir "${TSHDIR}"
 	destdir "/tmp"
 	execdir "/tmp"
 	bsd-objdir "/tmp"
@@ -239,7 +239,7 @@ if testcase "string interpolation"; then
 	} >"$CONFIG"
 	echo "\${kernel}" >"$STDIN"
 	robsd_config - <<-EOF
-	/tmp
+	${TSHDIR}
 	EOF
 fi
 
@@ -258,7 +258,7 @@ if testcase "list interpolation"; then
 	} >"$CONFIG"
 	echo "\${skip}" >"$STDIN"
 	robsd_config - <<-EOF
-	ROBSDDIR=/tmp
+	ROBSDDIR=${TSHDIR}
 	EOF
 fi
 
@@ -268,8 +268,8 @@ if testcase "hook"; then
 		echo "hook { \"echo\" \"\${robsddir}\" }"
 	} >"$CONFIG"
 	echo "\${hook}" >"$STDIN"
-	robsd_config - <<-'EOF'
-	echo /tmp
+	robsd_config - <<-EOF
+	echo ${TSHDIR}
 	EOF
 fi
 
@@ -289,20 +289,39 @@ if testcase "read only variables"; then
 	_arch="$(arch -s 2>/dev/null || arch)"
 	_machine="$(machine 2>/dev/null || arch)"
 
+	echo /builddir >"${TSHDIR}/.running"
 	default_config >"$CONFIG"
 	cat <<-'EOF' >"$STDIN"
 	${arch}
 	${machine}
 	${keep-dir}
+	${builddir}
 	EOF
 
 	robsd_config - <<-EOF
 	${_arch}
 	${_machine}
-	/tmp/attic
+	${TSHDIR}/attic
+	/builddir
 	EOF
 fi
 
+if testcase "builddir lock file missing"; then
+	: >"${TSHDIR}/.running"
+	default_config >"$CONFIG"
+	echo "\${builddir}" >"$STDIN"
+	robsd_config -e - <<-EOF
+	robsd-config: ${TSHDIR}/.running: line not found
+	EOF
+fi
+
+if testcase "builddir lock file empty"; then
+	default_config >"$CONFIG"
+	echo "\${builddir}" >"$STDIN"
+	robsd_config -e - <<-EOF
+	robsd-config: /dev/stdin:1: invalid substitution, unknown variable 'builddir'
+	EOF
+fi
 
 if testcase "invalid missing file"; then
 	robsd_config -e -- -f /nein >/dev/null

commit c5b3610ba2f097ac5edf78938c6e7fb1d3b7db71

keep the build lock while generating the report
Otherwise, robsd-ls won't be able to filter out itself since it reads the
current builddir from the lock file.

Regression introduced in commit 0e7f9df ("make builddir available during
robsd-config interpolation").

util.sh
@@ -1700,8 +1700,6 @@ trap_exit() {
 
 	_steps="$(step_path "$_builddir")"
 
-	lock_release "$_robsddir" "$_builddir" || :
-
 	# Generate the report if a step failed or the end step is reached.
 	if [ "$_err" -ne 0 ] ||
 	   step_eval -n end "$_steps" 2>/dev/null
@@ -1712,6 +1710,8 @@ trap_exit() {
 			sendmail root <"${_builddir}/report"
 		fi
 	fi
+
+	lock_release "$_robsddir" "$_builddir" || :
 
 	# Do not leave an empty build around.
 	[ -s "$_steps" ] || rm -r "$_builddir"