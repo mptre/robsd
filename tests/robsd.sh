mptre/robsd

commit d06d2e7a9b30856a41e8aea484683e6acead2152

invoke the hook for the end step after report creation

util.sh
@@ -1253,7 +1253,8 @@ robsd() {
 			# duration.
 			step_write -t -s "$_s" -n "$_name" -e 0 \
 				-d "$(duration_total -s "$_steps")" "$_steps"
-			robsd_hook -v "exit=0" -v "step=end"
+			# The hook is invoked as late as possible in the exit
+			# trap handler.
 			return 0
 		fi
 
@@ -1685,6 +1686,8 @@ trap_exit() {
 			sendmail root <"${_builddir}/report"
 		fi
 	fi
+
+	robsd_hook -v "exit=0" -v "step=end"
 
 	lock_release "$_robsddir" "$_builddir" || :
 

tests/robsd.sh
@@ -124,11 +124,10 @@ if testcase "basic"; then
 	robsd: step dmesg
 	robsd-hook: exec "sh" "${_hook}" "${_builddir}" "dmesg" "0"
 	robsd: step end
+	robsd: trap exit 0
 	robsd-hook: exec "sh" "${_hook}" "${_builddir}" "end" "0"
-	no report
 	stdout
 	stderr
-	robsd: trap exit 0
 	EOF
 fi
 

commit e2764e8698d51a490a527ffab8ce3b7d534560c6

fix end hook regression
Only invoke the hook for the end step iff it's present. Otherwise, robsd will
end up running the hook for the end step upon reboot.

Regression introduced in commit d06d2e7 ("invoke the hook for the end step after
report creation").

util.sh
@@ -1687,7 +1687,9 @@ trap_exit() {
 		fi
 	fi
 
-	robsd_hook -v "exit=0" -v "step=end"
+	if step_eval -n end "$_steps" 2>/dev/null; then
+		robsd_hook -v "exit=0" -v "step=end"
+	fi
 
 	lock_release "$_robsddir" "$_builddir" || :
 

tests/robsd.sh
@@ -85,7 +85,6 @@ if testcase "basic"; then
 		-e '/running as pid/d' \
 		-e '/^\+ /d' \
 		"$TMP1"
-	_user="$(logname)"
 	assert_file - "$TMP1" <<-EOF
 	robsd: using directory ${_builddir} at step 1
 	robsd: using diff ${TSHDIR}/src-one.diff rooted in ${TSHDIR}
@@ -135,6 +134,7 @@ if testcase "reboot"; then
 	robsd_config - <<-EOF
 	robsddir "${ROBSDDIR}"
 	execdir "${WRKDIR}/exec"
+	hook { "true" }
 	reboot yes
 	EOF
 	mkdir -p "$ROBSDDIR"
@@ -144,8 +144,27 @@ if testcase "reboot"; then
 	if [ -e "$_fail" ]; then
 		fail - "expected exit zero" <"$TMP1"
 	fi
-
 	_builddir="$(find "${ROBSDDIR}" -type d -mindepth 1 -maxdepth 1)"
+
+	sed -i \
+		-e '/running as pid/d' \
+		-e '/^\+ /d' \
+		"$TMP1"
+	assert_file - "$TMP1" <<-EOF
+	robsd: using directory ${_builddir} at step 1
+	robsd: step env
+	robsd-hook: exec "true"
+	robsd: step cvs
+	robsd-hook: exec "true"
+	robsd: step patch
+	robsd-hook: exec "true"
+	robsd: step kernel
+	robsd-hook: exec "true"
+	robsd: step reboot
+	robsd-hook: exec "true"
+	robsd: trap exit 0
+	EOF
+
 	if [ -e "${_builddir}/report" ]; then
 		fail - "expected no report" <"$TMP1"
 	fi