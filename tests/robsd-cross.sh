mptre/robsd

commit a7f980b1557b554bef8136851a3efc9e0e2ebe75

rework handling of cross target
Recent commit eed5866 ("add inet interpolation variable") broke robsd-crossenv
as the target variable ended up being absent. This in turn is caused by not
having a BUILDDIR around in robsd-crossenv.

Instead, let robsd-cross and robsd-crossenv pass the target.

util.sh
@@ -87,9 +87,10 @@ cleandir() {
 	done
 }
 
-# config_load
+# config_load [robsd-config-argument ...]
 #
 # Load and validate the configuration.
+# shellcheck disable=SC2120
 config_load() {
 	local _tmp
 
@@ -105,8 +106,8 @@ config_load() {
 	{
 		cat
 		echo "EXECDIR=\${execdir}"
-	} | "$ROBSDCONFIG" -m "$_MODE" -b "${BUILDDIR}" \
-		${ROBSDCONF:+"-f${ROBSDCONF}"} - >"$_tmp" || return 1
+	} | "$ROBSDCONFIG" -m "$_MODE" ${ROBSDCONF:+"-f${ROBSDCONF}"} "$@" - \
+		>"$_tmp" || return 1
 	eval "$(<"$_tmp")"
 	rm "$_tmp"
 

Makefile
@@ -171,6 +171,7 @@ DISTFILES+=	tests/report-skip.sh
 DISTFILES+=	tests/report.sh
 DISTFILES+=	tests/robsd-config.sh
 DISTFILES+=	tests/robsd-cross.sh
+DISTFILES+=	tests/robsd-crossenv.sh
 DISTFILES+=	tests/robsd-hook.sh
 DISTFILES+=	tests/robsd-ports.sh
 DISTFILES+=	tests/robsd-regress.sh

config.c
@@ -139,7 +139,6 @@ struct config {
 	struct variable_list	 cf_variables;
 	const struct grammar	*cf_grammar;
 	const char		*cf_path;
-	const char		*cf_builddir;
 	enum {
 		CONFIG_ROBSD,
 		CONFIG_ROBSD_CROSS,
@@ -170,8 +169,7 @@ static int			 config_present(const struct config *,
 
 static int	config_validate_directory(const struct config *, const char *);
 
-static char	*crosstarget(const char *);
-static int	 regressname(char *, size_t, const char *, const char *);
+static int	regressname(char *, size_t, const char *, const char *);
 
 static const struct grammar robsd[] = {
 	{ "robsddir",		DIRECTORY,	config_parse_string,	REQ,	NULL },
@@ -314,12 +312,6 @@ config_free(struct config *cf)
 	free(cf);
 }
 
-void
-config_set_builddir(struct config *cf, const char *builddir)
-{
-	cf->cf_builddir = builddir;
-}
-
 int
 config_parse(struct config *cf, const char *path)
 {
@@ -330,6 +322,28 @@ config_parse(struct config *cf, const char *path)
 	if (config_exec(cf))
 		return 1;
 	return 0;
+}
+
+int
+config_append_var(struct config *cf, const char *str)
+{
+	char *name, *val;
+	int error;
+
+	val = strchr(str, '=');
+	if (val == NULL) {
+		warnx("missing variable separator in '%s'", str);
+		return 1;
+	}
+	name = strndup(str, val - str);
+	if (name == NULL)
+		err(1, NULL);
+	val++;	/* consume '=' */
+	error = config_append_string(cf, name, val);
+	if (error)
+		warnx("variable '%s' cannot be defined", name);
+	free(name);
+	return error;
 }
 
 int
@@ -1137,17 +1151,6 @@ config_append_defaults(struct config *cf)
 {
 	char *str;
 
-	if (cf->cf_mode == CONFIG_ROBSD_CROSS &&
-	    cf->cf_builddir != NULL) {
-		/*
-		 * Ignore errors as the configuration is loaded before the
-		 * target file is created.
-		 */
-		str = crosstarget(cf->cf_builddir);
-		if (str != NULL)
-			config_append(cf, STRING, "target", str, 0);
-	}
-
 	if (cf->cf_mode == CONFIG_ROBSD_REGRESS) {
 		str = ifgrinet("egress");
 		if (str == NULL)
@@ -1274,54 +1277,6 @@ config_validate_directory(const struct config *cf, const char *name)
 	free(path);
 
 	return error;
-}
-
-/*
- * Read the cross target from the file located in the build directory created by
- * robsd-cross.
- */
-static char *
-crosstarget(const char *builddir)
-{
-	char path[PATH_MAX];
-	char *buf = NULL;
-	char *target = NULL;
-	FILE *fh;
-	ssize_t pathsiz;
-	size_t bufsiz = 0;
-	size_t len;
-	int n;
-
-	pathsiz = sizeof(path);
-	n = snprintf(path, pathsiz, "%s/target", builddir);
-	if (n < 0 || n >= pathsiz) {
-		warnc(ENAMETOOLONG, "%s", __func__);
-		return NULL;
-	}
-
-	fh = fopen(path, "r");
-	if (fh == NULL) {
-		if (errno != ENOENT)
-			warn("%s", path);
-		return NULL;
-	}
-	if (getline(&buf, &bufsiz, fh) == -1) {
-		warn("%s", path);
-		goto out;
-	}
-
-	len = strlen(buf);
-	if (len == 0) {
-		warnx("%s: empty", path);
-		free(buf);
-		goto out;
-	}
-	buf[len - 1] = '\0';
-	target = buf;
-
-out:
-	fclose(fh);
-	return target;
 }
 
 static int

extern.h
@@ -10,8 +10,8 @@
 
 struct config	*config_alloc(const char *);
 void		 config_free(struct config *);
-void		 config_set_builddir(struct config *, const char *);
 int		 config_parse(struct config *, const char *);
+int		 config_append_var(struct config *, const char *);
 int		 config_append_string(struct config *, const char *,
     const char *);
 struct variable	*config_find(struct config *, const char *);

robsd-config.c
@@ -25,14 +25,8 @@ main(int argc, char *argv[])
 	if (pledge("stdio rpath inet getpw route", NULL) == -1)
 		err(1, "pledge");
 
-	while ((ch = getopt(argc, argv, "b:f:m:")) != -1) {
+	while ((ch = getopt(argc, argv, "f:m:v:")) != -1) {
 		switch (ch) {
-		case 'b':
-			if (config == NULL)
-				usage();
-			config_set_builddir(config, optarg);
-			break;
-
 		case 'f': {
 			int n;
 
@@ -52,6 +46,14 @@ main(int argc, char *argv[])
 		case 'm':
 			config = config_alloc(optarg);
 			if (config == NULL) {
+				error = 1;
+				goto out;
+			}
+			break;
+
+		case 'v':
+			if (config == NULL ||
+			    config_append_var(config, optarg)) {
 				error = 1;
 				goto out;
 			}
@@ -91,7 +93,7 @@ out:
 static __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: robsd-config -m mode [-b build-dir] [-f file] "
+	fprintf(stderr, "usage: robsd-config -m mode [-f file] [-v var=val]"
 	    "[-]\n");
 	exit(1);
 }

robsd-cross
@@ -15,13 +15,22 @@ usage() {
 	exit 1
 }
 
+# target arg ...
+#
+# Get the target argument, which is expected to be the last one.
+target() (
+	shift $(($# - 1))
+	echo "$1"
+)
+
 . "${EXECDIR:-/usr/local/libexec/robsd}/util.sh"
 
 setmode "robsd-cross"
 setprogname "robsd-cross"
 [ "$(id -u)" -ne 0 ] && fatal "must be run as root"
 
-config_load <<'EOF'
+_target="$(target "$@")"
+config_load -v "target=${_target}" <<'EOF'
 ROBSDDIR="${robsddir}"
 KEEP="${keep}"
 SKIP="${skip}"

robsd-cross-dirs.sh
@@ -1,6 +1,7 @@
 . "${EXECDIR}/util.sh"
 
-config_load <<'EOF'
+_target="$(<"${BUILDDIR}/target")"
+config_load -v "target=${_target}" <<'EOF'
 CROSSDIR="${crossdir}"
 BSDSRCDIR="${bsd-srcdir}"
 TARGET="${target}"

robsd-cross-distrib.sh
@@ -1,6 +1,7 @@
 . "${EXECDIR}/util.sh"
 
-config_load <<'EOF'
+_target="$(<"${BUILDDIR}/target")"
+config_load -v "target=${_target}" <<'EOF'
 CROSSDIR="${crossdir}"
 BSDSRCDIR="${bsd-srcdir}"
 TARGET="${target}"

commit 2801d533aed10554023164b30f2859672ea1da55

fix absent target architecture in robsd-cross report subject
Regression introduced in commit a7f980b ("rework handling of cross target").

util-cross.sh
@@ -1,8 +1,19 @@
-# cross_report_subject
+# cross_report_subject -b build-dir
 #
 # Get report subject prefix, including the target.
 cross_report_subject() {
-	printf '%s.%s: ' "$(machine)" "$(config_value target)"
+	local _builddir
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		-b)	shift; _builddir="$1";;
+		*)	break;;
+		esac
+		shift
+	done
+	: "${_builddir:?}"
+
+	printf '%s.%s: ' "$(machine)" "$(<"${_builddir}/target")"
 }
 
 # cross_steps

util.sh
@@ -900,7 +900,7 @@ report() {
 	{
 		printf 'Subject: %s: %s: ' "$_MODE" "$(hostname -s)"
 		case "$_MODE" in
-		robsd-cross)	cross_report_subject;;
+		robsd-cross)	cross_report_subject -b "$_builddir";;
 		*)		;;
 		esac
 		printf '%s\n\n' "$_status"

tests/robsd-cross.sh
@@ -38,6 +38,11 @@ EOF
 	assert_file - "${_builddir}/target" <<-EOF
 	amd64
 	EOF
+
+	grep Subject "${_builddir}/report" >"$TMP1"
+	assert_file - "$TMP1" <<-EOF
+	Subject: robsd-cross: $(hostname -s): $(machine).amd64: ok
+	EOF
 fi
 
 if testcase "missing arguments"; then