mptre/robsd

commit 7574a28f58e29504b27f5647360834725796253a

favor config_append_string()

conf.c
@@ -1168,12 +1168,12 @@ out:
 static struct variable *
 config_default_inet(struct config *cf)
 {
-	union variable_value val;
+	char *inet;
 
-	val.str = ifgrinet("egress");
-	if (val.str == NULL)
+	inet = ifgrinet("egress");
+	if (inet == NULL)
 		return NULL;
-	return config_append(cf, STRING, "inet", &val, 0, VARIABLE_FLAG_DIRTY);
+	return config_append_string(cf, "inet", inet);
 }
 
 static struct variable *

commit 14246c49f280c09420e133730c66695686b75d82

plug memory leak
Introduced in commit 7574a28 ("favor config_append_string()").

conf.c
@@ -1181,7 +1181,8 @@ config_default_inet(struct config *cf)
 	inet = ifgrinet("egress");
 	if (inet == NULL)
 		return NULL;
-	return config_append_string(cf, "inet", inet);
+	return config_append(cf, STRING, "inet",
+	    &(union variable_value){.str = inet}, 0, VARIABLE_FLAG_DIRTY);
 }
 
 static struct variable *