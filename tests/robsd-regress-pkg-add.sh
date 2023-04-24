cb7904d55b89237a62e08280146e1e0ac5a7da59
Anton Lindqvist <anton@basename.se>
2023-04-24 07:45:33 +0200

    tests: add dedicate tests for step robsd-regress-pkg-add.sh

---
+++ tests/robsd-regress-pkg-add.sh
@@ -0,0 +1,31 @@
+portable no
+
+_step="${EXECDIR}/robsd-regress-pkg-add.sh"
+
+if testcase "basic"; then
+	robsd_config -R - <<-EOF
+	robsddir "${TSHDIR}"
+	regress "one" packages { "uniq" "dup" }
+	regress "two" packages { "dup" }
+	EOF
+	_builddir="${TSHDIR}/2022-11-21"
+	echo "$_builddir" >"${TSHDIR}/.running"
+	mkdir -p "${_builddir}/tmp"
+
+	cat <<-EOF >"${TSHDIR}/pkg_add"
+	exit 0
+	EOF
+	chmod u+x "${TSHDIR}/pkg_add"
+
+	if ! (setmode "robsd-regress" &&
+	      env PATH="${TSHDIR}:${PATH}" sh -eux -o pipefail "$_step") \
+	     >"$TMP1" 2>&1; then
+		fail - "expected exit zero" <"$TMP1"
+	fi
+
+	assert_file - "${_builddir}/tmp/packages" <<-EOF
+	uniq
+	dup
+	dup
+	EOF
+fi
