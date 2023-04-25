mptre/robsd

commit e85e14568829e894d2ce50dbefb9e78b8faee1bd

remove needless glob(3) gl_matchc compat
This field is mainly intended to distinguish between the number of matches for
the current invocation and the accumulated one. Since we only invoke glob(3)
once per state, they will always be equal.

conf.c
@@ -900,7 +900,7 @@ config_parse_glob(struct config *cf, struct variable_value *val)
 	}
 
 	variable_value_init(val, LIST);
-	for (i = 0; i < g.gl_matchc; i++) {
+	for (i = 0; i < g.gl_pathc; i++) {
 		char *str;
 
 		str = estrdup(g.gl_pathv[i]);

configure
@@ -87,17 +87,6 @@ check_attribute_fallthrough() {
 	EOF
 }
 
-check_glob_matchc() {
-	compile <<-EOF
-	#include <glob.h>
-
-	int main(void) {
-		glob_t g;
-		return g.gl_matchc;
-	}
-	EOF
-}
-
 # Check if the compiler implicitly supports GNU C99.
 check_gnu99() {
 	compile <<-EOF
@@ -225,7 +214,6 @@ set -u
 set -x
 
 HAVE_ATTRIBUTE_FALLTHROUGH=0
-HAVE_GLOB_MATCHC=0
 HAVE_GNU_SOURCE=0
 HAVE_MACHINE=0
 HAVE_MACHINE_ARCH=0
@@ -262,7 +250,6 @@ if [ "$_pedantic" = 1 ]; then
 fi
 
 check_attribute_fallthrough && HAVE_ATTRIBUTE_FALLTHROUGH=1
-check_glob_matchc && HAVE_GLOB_MATCHC=1
 check_gnu_source && HAVE_GNU_SOURCE=1
 check_machine && HAVE_MACHINE=1
 check_machine_arch && HAVE_MACHINE_ARCH=1
@@ -298,8 +285,6 @@ if [ $HAVE_ATTRIBUTE_FALLTHROUGH -eq 1 ]; then
 else
 	printf '#define FALLTHROUGH do {} while (0) /* FALLTHROUGH */\n'
 fi
-
-[ $HAVE_GLOB_MATCHC -eq 0 ] && printf '#define gl_matchc gl_pathc\n'
 
 [ $HAVE_PLEDGE -eq 0 ] &&
 	printf 'int pledge(const char *, const char *);\n'

commit aedb0402c20456a80dd9d1e329cc179988f64082

fix glob(3) no matches regression
Favoring gl_pathc over gl_matchc is not the same when GLOB_NOCHECK is in play as
the pattern would be added in case there's no matches.

Regression introduced in commit e85e145 ("remove needless glob(3) gl_matchc
compat").

conf.c
@@ -923,13 +923,18 @@ config_parse_glob(struct config *cf, struct variable_value *val)
 
 	if (!lexer_expect(cf->cf_lx, TOKEN_STRING, &tk))
 		return 1;
-	error = glob(tk->tk_str, GLOB_NOCHECK, NULL, &g);
-	if (error) {
-		lexer_warn(cf->cf_lx, tk->tk_lno, "glob: %d", error);
-		goto out;
-	}
 
 	variable_value_init(val, LIST);
+
+	error = glob(tk->tk_str, 0, NULL, &g);
+	if (error) {
+		if (error == GLOB_NOMATCH)
+			return 0;
+
+		lexer_warn(cf->cf_lx, tk->tk_lno, "glob: %d", error);
+		return error;
+	}
+
 	for (i = 0; i < g.gl_pathc; i++) {
 		char *str;
 
@@ -937,7 +942,6 @@ config_parse_glob(struct config *cf, struct variable_value *val)
 		*VECTOR_ALLOC(val->list) = str;
 	}
 
-out:
 	globfree(&g);
 	return 0;
 }

tests/robsd-config.sh
@@ -289,14 +289,15 @@ if testcase "list interpolation"; then
 fi
 
 if testcase "glob"; then
-	touch "${TSHDIR}/one.diff" "${TSHDIR}/two.diff"
+	touch "${TSHDIR}/src-one.diff" "${TSHDIR}/src-two.diff"
 	{
 		default_config
-		echo "bsd-diff \"${TSHDIR}/*.diff\""
+		echo "bsd-diff \"${TSHDIR}/src-*.diff\""
+		echo "x11-diff \"${TSHDIR}/x11-*.diff\""
 	} >"$CONFIG"
-	echo "\${bsd-diff}" >"$STDIN"
+	echo "'\${bsd-diff}' '\${x11-diff}'" >"$STDIN"
 	robsd_config - <<-EOF
-	${TSHDIR}/one.diff ${TSHDIR}/two.diff
+	'${TSHDIR}/src-one.diff ${TSHDIR}/src-two.diff' ''
 	EOF
 fi
 