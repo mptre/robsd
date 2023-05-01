mptre/robsd

commit e47ff9fa69b3931dca702f005a60c43257758ca2

favor robsd-ls in purge()

util.sh
@@ -798,11 +798,9 @@ purge() {
 
 	_attic="$(config_value keep-dir)"
 
-	find "$_dir" -mindepth 1 -maxdepth 1 -type d |
-	grep -v "$_attic" |
+	prev_release -B |
 	sort -n |
-	tail -r |
-	tail -n "+$((_n + 1))" |
+	head -n "$_n" |
 	while read -r _d; do
 		[ -d "$_attic" ] || mkdir "$_attic"
 

tests/purge.sh
@@ -1,8 +1,7 @@
 portable no
 
 if testcase "basic"; then
-	# shellcheck disable=SC2086
-	mkdir -p ${ROBSDDIR}/2019-03-0{1,2}/rel
+	mkdir -p "${ROBSDDIR}/2019-03-01/rel" "${ROBSDDIR}/2019-03-02/rel"
 	for _d in "$ROBSDDIR"/*; do
 		for _f in \
 			01-base.log 01-base.log.1 03-env.log comment dmesg \
@@ -42,8 +41,7 @@ if testcase "basic"; then
 fi
 
 if testcase "missing log files"; then
-	# shellcheck disable=SC2086
-	mkdir -p ${ROBSDDIR}/2019-03-0{1,2}/rel
+	mkdir -p "${ROBSDDIR}/2019-03-01/rel" "${ROBSDDIR}/2019-03-02/rel"
 	robsd_config - <<-EOF
 	robsddir "${TSHDIR}"
 	EOF
@@ -53,8 +51,7 @@ if testcase "missing log files"; then
 fi
 
 if testcase "attic already present"; then
-	# shellcheck disable=SC2086
-	mkdir -p ${ROBSDDIR}/2019-03-0{1,2}
+	mkdir -p "${ROBSDDIR}/2019-03-01/rel" "${ROBSDDIR}/2019-03-02/rel"
 	mkdir -p "${ROBSDDIR}/attic"
 	robsd_config - <<-EOF
 	robsddir "${TSHDIR}"

commit f01480342c24375217c2c793f3720b55972dd846

fix robsd-clean regression
Partial revert as `tail -n +N` is required to remove the oldest directories.
Regression introduced in commit e47ff9f ("favor robsd-ls in purge()").

util.sh
@@ -799,8 +799,8 @@ purge() {
 	_attic="$(config_value keep-dir)"
 
 	prev_release -B |
-	sort -n |
-	head -n "$_n" |
+	sort -nr |
+	tail -n "+$((_n + 1))" |
 	while read -r _d; do
 		[ -d "$_attic" ] || mkdir "$_attic"
 

tests/purge.sh
@@ -1,7 +1,9 @@
 portable no
 
 if testcase "basic"; then
-	mkdir -p "${ROBSDDIR}/2019-03-01/rel" "${ROBSDDIR}/2019-03-02/rel"
+	for _d in 2019-03-01 2019-03-02 2019-03-03; do
+		mkdir -p "${ROBSDDIR}/${_d}/rel"
+	done
 	for _d in "$ROBSDDIR"/*; do
 		for _f in \
 			01-base.log 01-base.log.1 03-env.log comment dmesg \
@@ -18,13 +20,17 @@ if testcase "basic"; then
 	robsddir "${TSHDIR}"
 	EOF
 
-	assert_eq "${ROBSDDIR}/2019-03-01" "$(purge "$ROBSDDIR" 1)"
-
-	[ -d "${ROBSDDIR}/2019-03-02" ] ||
-		fail "expected 2019-03-02 to be left"
+	purge "$ROBSDDIR" 2 >"$TMP1"
+	assert_file - "$TMP1" <<-EOF
+	${ROBSDDIR}/2019-03-01
+	EOF
 
 	[ -d "${ROBSDDIR}/attic/2019/03/01" ] ||
 		fail "expected 2019-03-01 to be moved"
+	[ -d "${ROBSDDIR}/2019-03-02" ] ||
+		fail "expected 2019-03-02 to be left"
+	[ -d "${ROBSDDIR}/2019-03-03" ] ||
+		fail "expected 2019-03-03 to be left"
 
 	for _f in 01-base.log 01-base.log.1 03-env.log dmesg tmp; do
 		_p="${ROBSDDIR}/attic/2019/03/01/${_f}"