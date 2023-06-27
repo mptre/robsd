c5680e3a2cb221607aa904aafd3ad9044e7b4eb1
Anton Lindqvist <anton@basename.se>
2023-06-27 06:51:58 +0200

    redirect pkg_info stderr to /dev/null
    Gets rid of confusing "Can't find $pkg" messages from install log.

--- robsd-regress-pkg-add.sh
+++ robsd-regress-pkg-add.sh
@@ -10,7 +10,7 @@ _tmpdir="${BUILDDIR}/tmp"
 { config_value regress-packages 2>/dev/null || :; } |
 xargs printf '%s\n' | sort | uniq |
 while read -r _p; do
-	if ! PKG_PATH='' pkg_info "$_p" >/dev/null; then
+	if ! PKG_PATH='' pkg_info "$_p" >/dev/null 2>&1; then
 		echo "$_p" >>"${_tmpdir}/packages"
 	fi
 done
