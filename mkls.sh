be055d0d00e3eb95351059ad21c669ae16434fd3
Anton Lindqvist <anton@basename.se>
2023-06-30 14:10:12 +0200

    use git-archive(1) in make dist target

--- mkls.sh
+++ mkls.sh
@@ -1,31 +1,10 @@
-export LC_ALL=C
-
-mkls "$@" \
-SRCS \
-	!(fuzz-*|robsd-*).c \
-	$(find libks -type f -name '*.c' -exec basename {} \;) \
-	-- \
-KNFMT \
-	!(compat-*).c !(config).h \
-	-- \
-CLANGTIDY \
-	!(config|compat-*).h !(compat-*).c \
-	-- \
-CPPCHECK \
-	!(compat-*).c \
-	-- \
-SCRIPTS \
-	!(mkls).sh \
-	-- \
-DISTFILES \
-	*.c !(config).h libks/*.[ch] *.md !(mkls).sh \
-	configure \
-	robsd?(-clean|-cross|-crossenv|-kill|-ports|-regress|-rescue) \
-	*.5 robsd?(-clean|-config|-cross|-crossenv|-kill|-ports|-regress|-regress-html|-regress-log|-rescue|-stat|-step).[0-9] \
-	LICENSE Makefile Makefile.inc \
-	tests/*.sh tests/Makefile
+mkls -s "$@" -- \
+SRCS		!(fuzz-*|robsd-*).c $(cd libks && ls *.c) -- \
+KNFMT		!(compat-*).c !(config).h -- \
+CLANGTIDY	!(config|compat-*).h !(compat-*).c -- \
+CPPCHECK	!(compat-*).c -- \
+SCRIPTS		!(mkls).sh
 
 cd tests
-mkls "$@" \
-TESTS \
-	!(t|util).sh
+mkls -s "$@" -- \
+TESTS	!(t|util).sh
