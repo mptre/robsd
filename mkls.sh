7bce2898d037bd3bee0c2e4e31e04764b02d486c
Anton Lindqvist <anton@basename.se>
2023-08-28 06:43:46 +0200

    explicitly enumerate required source files per binary
    Getting annoyed with relinking all binaries upon changing a single source file.

--- mkls.sh
+++ mkls.sh
@@ -1,5 +1,5 @@
 mkls -s "$@" -- \
-SRCS		!(fuzz-*|robsd-*).c $(cd libks && ls *.c) -- \
+COMPATS		compat-*.c -- \
 KNFMT		!(compat-*).c !(config).h -- \
 CLANGTIDY	!(config|compat-*).h !(compat-*).c -- \
 CPPCHECK	!(compat-*).c -- \
