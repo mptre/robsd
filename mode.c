ad1e664339e88534da13cc7c864bea33020f3de0
Anton Lindqvist <anton@basename.se>
2023-08-28 06:43:47 +0200

    use X macro for robsd_mode

--- mode.c
+++ mode.c
@@ -7,31 +7,25 @@
 int
 robsd_mode_parse(const char *mode, enum robsd_mode *res)
 {
-	if (strcmp(mode, "robsd") == 0)
-		*res = ROBSD;
-	else if (strcmp(mode, "robsd-cross") == 0)
-		*res = ROBSD_CROSS;
-	else if (strcmp(mode, "robsd-ports") == 0)
-		*res = ROBSD_PORTS;
-	else if (strcmp(mode, "robsd-regress") == 0)
-		*res = ROBSD_REGRESS;
-	else
-		return 1;
-	return 0;
+#define OP(c, s) do {						\
+	if (strcmp(mode, (s)) == 0) {				\
+		*res = (c);					\
+		return 0;					\
+	}							\
+} while (0);
+	FOR_ROBSD_MODES(OP)
+#undef OP
+
+	return 1;
 }
 
 const char *
 robsd_mode_str(enum robsd_mode mode)
 {
 	switch (mode) {
-	case ROBSD:
-		return "robsd";
-	case ROBSD_CROSS:
-		return "robsd-cross";
-	case ROBSD_PORTS:
-		return "robsd-ports";
-	case ROBSD_REGRESS:
-		return "robsd-regress";
+#define OP(c, s) case c: return s;
+	FOR_ROBSD_MODES(OP)
+#undef OP
 	}
 	return "unknown";
 }
--- mode.h
+++ mode.h
@@ -1,11 +1,16 @@
 #ifndef MODE_H
 #define MODE_H
 
+#define FOR_ROBSD_MODES(OP)						\
+	OP(ROBSD,	  "robsd")					\
+	OP(ROBSD_CROSS,	  "robsd-cross")				\
+	OP(ROBSD_PORTS,	  "robsd-ports")				\
+	OP(ROBSD_REGRESS, "robsd-regress")
+
 enum robsd_mode {
-	ROBSD,
-	ROBSD_CROSS,
-	ROBSD_PORTS,
-	ROBSD_REGRESS,
+#define OP(c, _) c,
+	FOR_ROBSD_MODES(OP)
+#undef OP
 };
 
 int		 robsd_mode_parse(const char *, enum robsd_mode *);
