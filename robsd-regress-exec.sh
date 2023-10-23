1a4bcf6f41ee50593375f731727ee75f0f42c4c9
Anton Lindqvist <anton@basename.se>
2023-10-23 07:03:12 +0200

    robsd-regress: remove unused REGRESS_LOG

--- robsd-regress-exec.sh
+++ robsd-regress-exec.sh
@@ -8,20 +8,15 @@ BSDSRCDIR="${bsd-srcdir}"
 REGRESSUSER="${regress-user}"
 EOF
 
-_err=0
-_log="${BUILDDIR}/tmp/regress"; : >"$_log"; chmod 666 "$_log"
-_env="REGRESS_LOG=${_log} REGRESS_FAIL_EARLY=no"
+_env="REGRESS_FAIL_EARLY=no"
 _env="env ${_env} $(config_value "regress-${1}-env" 2>/dev/null || :)"
 _target="$(config_value "regress-${1}-target")"
 _make="${_env} make -C ${BSDSRCDIR}/regress/${1} ${_target}"
 if regress_root "$1"; then
-	$_make || _err="$?"
+	$_make
 else
 	export SUDO
 	# Since we're most likely running as the build user, use a more generous
 	# login class as some regression tests are resource hungry.
-	unpriv -c staff "$REGRESSUSER" "exec ${_make}" || _err="$?"
+	unpriv -c staff "$REGRESSUSER" "exec ${_make}"
 fi
-
-rm -f "$_log"
-exit "$_err"
