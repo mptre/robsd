b120b2f06e81c943785ba1d5d2dc85fa048d7a44
Anton Lindqvist <anton@basename.se>
2023-10-23 07:03:14 +0200

    fuzz-config: cope with latest changes

--- fuzz-config.c
+++ fuzz-config.c
@@ -2,11 +2,14 @@
 
 #include <stddef.h>
 
+#include "libks/arena.h"
+
 #include "conf.h"
 
 int
 main(int argc, char *argv[])
 {
+	struct arena *arena, *scratch;
 	struct config *config = NULL;
 	const char *mode = "robsd";
 	int error;
@@ -14,10 +17,16 @@ main(int argc, char *argv[])
 	if (argc == 2)
 		mode = argv[1];
 
-	config = config_alloc(mode, "/dev/stdin");
+	arena = arena_alloc(ARENA_FATAL);
+	arena_scope(arena, eternal);
+	scratch = arena_alloc(ARENA_FATAL);
+
+	config = config_alloc(mode, "/dev/stdin", &eternal, scratch);
 	if (config == NULL)
 		return 1;
 	error = config_parse(config);
 	config_free(config);
+	arena_free(scratch);
+	arena_free(arena);
 	return error;
 }
