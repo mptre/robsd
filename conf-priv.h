a13fdc3b614c96fb88af1a8f85e24b803f2fc73c
Anton Lindqvist <anton@basename.se>
2024-05-31 16:05:15 +0200

    conf: move struct grammar definition to conf-priv.h

--- conf-priv.h
+++ conf-priv.h
@@ -1,6 +1,7 @@
 #include <stddef.h>	/* size_t */
 
 #include "mode.h"
+#include "variable-value.h"
 
 struct config {
 	struct arena_scope		 *eternal;
@@ -32,6 +33,27 @@ struct config {
 
 struct config_callbacks {
 	int	(*init)(struct config *);
+};
+
+struct grammar {
+	const char		*gr_kw;
+	enum variable_type	 gr_type;
+	int			 (*gr_fn)(struct config *,
+	    struct variable_value *);
+	unsigned int		 gr_flags;
+#define REQ	0x00000001u	/* required */
+#define REP	0x00000002u	/* may be repeated */
+#define PAT	0x00000004u	/* fnmatch(3) keyword fallback */
+#define FUN	0x00000008u	/* default obtain through function call */
+#define EARLY	0x00000010u	/* interpolate early */
+
+	union {
+		const void	*ptr;
+		struct variable	*(*fun)(struct config *, const char *);
+		int		 i32;
+#define D_FUN(x)	.fun = (x)
+#define D_I32(x)	.i32 = (x)
+	} gr_default;
 };
 
 const struct config_callbacks	*config_robsd_callbacks(void);
--- conf.c
+++ conf.c
@@ -97,27 +97,6 @@ static void	variable_value_append(struct variable_value *, const char *);
  * grammar ---------------------------------------------------------------------
  */
 
-struct grammar {
-	const char		*gr_kw;
-	enum variable_type	 gr_type;
-	int			 (*gr_fn)(struct config *,
-	    struct variable_value *);
-	unsigned int		 gr_flags;
-#define REQ	0x00000001u	/* required */
-#define REP	0x00000002u	/* may be repeated */
-#define PAT	0x00000004u	/* fnmatch(3) keyword fallback */
-#define FUN	0x00000008u	/* default obtain through function call */
-#define EARLY	0x00000010u	/* interpolate early */
-
-	union {
-		const void	*ptr;
-		struct variable	*(*fun)(struct config *, const char *);
-		int		 i32;
-#define D_FUN(x)	.fun = (x)
-#define D_I32(x)	.i32 = (x)
-	} gr_default;
-};
-
 static const struct grammar	*config_find_grammar_for_keyword(
     const struct config *, const char *);
 static const struct grammar	*config_find_grammar_for_interpolation(
