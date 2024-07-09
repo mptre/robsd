96d2b8598d307fe8177ce7807323956aa1c50afd
Anton Lindqvist <anton@basename.se>
2024-07-09 10:12:09 +0200

    alloc: remove unused ecalloc() function
    Tool: cppcheck

--- alloc.c
+++ alloc.c
@@ -6,17 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *
-ecalloc(size_t nmemb, size_t size)
-{
-	void *p;
-
-	p = calloc(nmemb, size);
-	if (p == NULL)
-		err(1, NULL);
-	return p;
-}
-
 char *
 estrdup(const char *str)
 {
--- alloc.h
+++ alloc.h
@@ -1,6 +1,4 @@
 #include <stddef.h>	/* size_t */
 
-void	*ecalloc(size_t, size_t);
-
 char	*estrdup(const char *);
 char	*estrndup(const char *, size_t);
