2ced6014192e92a4e2ddbde95fa5991fffc21f36
Anton Lindqvist <anton@basename.se>
2025-06-27 15:13:37 +0200

    break long comment

--- util.sh
+++ util.sh
@@ -1358,7 +1358,8 @@ step_value() {
 
 # trap_exit -r robsd-dir [-b build-dir] [-s stat-pid]
 #
-# Exit trap handler. The build dir may not be present if we failed very early on.
+# Exit trap handler. The build dir may not be present if we failed very early
+# on.
 trap_exit() {
 	local _err="$?"
 	local _builddir=""
